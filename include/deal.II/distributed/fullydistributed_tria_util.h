7ce7068a5366bc4b864126b39d83e4eaa5269b27
Peter Munch <peterrmuench@gmail.com>
2019-08-17 14:48:25 +0200

    Add p:f:t

---
+++ include/deal.II/distributed/fullydistributed_tria_util.h
@@ -0,0 +1,478 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2008 - 2019 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+#ifndef dealii_fullydistributed_tria_util_h
+#define dealii_fullydistributed_tria_util_h
+
+#include <deal.II/base/geometry_info.h>
+#include <deal.II/base/mpi.h>
+
+#include <deal.II/distributed/fullydistributed_tria.h>
+#include <deal.II/distributed/tria.h>
+
+#include <deal.II/dofs/dof_accessor.h>
+#include <deal.II/dofs/dof_handler.h>
+
+#include <deal.II/grid/grid_tools.h>
+
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
+#include <fstream>
+#include <functional>
+
+DEAL_II_NAMESPACE_OPEN
+
+namespace parallel
+{
+  namespace fullydistributed
+  {
+    namespace Utilities
+    {
+      template <typename CELL>
+      void
+      set_flag_reverse(CELL cell)
+      {
+        cell->set_user_flag();
+        if (cell->level() != 0)
+          set_flag_reverse(cell->parent());
+      }
+
+
+      template <int dim>
+      unsigned int
+      convert_binary_to_gid(
+        const std::array<unsigned int, 4> binary_representation)
+      {
+        const unsigned int coarse_cell_id = binary_representation[0];
+
+        const unsigned int n_child_indices = binary_representation[1] >> 2;
+
+        const unsigned int children_per_value =
+          sizeof(CellId::binary_type::value_type) * 8 / dim;
+        unsigned int child_level  = 0;
+        unsigned int binary_entry = 2;
+
+        std::vector<unsigned int> cell_indices;
+
+        // Loop until all child indices have been written
+        while (child_level < n_child_indices)
+          {
+            Assert(binary_entry < binary_representation.size(),
+                   ExcInternalError());
+
+            for (unsigned int j = 0; j < children_per_value; ++j)
+              {
+                // const unsigned int child_index =
+                //  static_cast<unsigned int>(child_indices[child_level]);
+                // Shift the child index to its position in the unsigned int
+                // and store it
+                unsigned int cell_index =
+                  (((binary_representation[binary_entry] >> (j * dim))) &
+                   (GeometryInfo<dim>::max_children_per_cell - 1));
+                cell_indices.push_back(cell_index);
+                ++child_level;
+                if (child_level == n_child_indices)
+                  break;
+              }
+            ++binary_entry;
+          }
+
+        unsigned int temp = coarse_cell_id;
+        for (auto i : cell_indices)
+          {
+            temp = temp * GeometryInfo<dim>::max_children_per_cell + i;
+          }
+
+        return temp;
+      }
+
+
+      template <int dim, int spacedim = dim>
+      ConstructionData<dim, spacedim>
+      copy_from_triangulation(
+        const dealii::Triangulation<dim, spacedim> &tria,
+        const Triangulation<dim, spacedim> &        tria_pft,
+        const unsigned int my_rank_in = numbers::invalid_unsigned_int)
+      {
+        const MPI_Comm comm = tria_pft.get_communicator();
+
+        if (auto tria_pdt = dynamic_cast<
+              const parallel::distributed::Triangulation<dim, spacedim> *>(
+              &tria))
+          AssertThrow(comm == tria_pdt->get_communicator(),
+                      ExcMessage("MPI communicators do not match."));
+
+        unsigned int my_rank = my_rank_in;
+        AssertThrow(my_rank == numbers::invalid_unsigned_int ||
+                      my_rank < dealii::Utilities::MPI::n_mpi_processes(comm),
+                    ExcMessage(
+                      "Rank has to be smaller than available processes."));
+
+        if (auto tria_pdt = dynamic_cast<
+              const parallel::distributed::Triangulation<dim, spacedim> *>(
+              &tria))
+          {
+            if (my_rank == numbers::invalid_unsigned_int ||
+                my_rank == dealii::Utilities::MPI::this_mpi_process(comm))
+              my_rank = dealii::Utilities::MPI::this_mpi_process(comm);
+            else
+              AssertThrow(false,
+                          ExcMessage("PDT: y_rank has to equal global rank."));
+          }
+        else if (auto tria_serial =
+                   dynamic_cast<const dealii::Triangulation<dim, spacedim> *>(
+                     &tria))
+          {
+            if (my_rank == numbers::invalid_unsigned_int)
+              my_rank = dealii::Utilities::MPI::this_mpi_process(comm);
+          }
+        else
+          {
+            AssertThrow(false,
+                        ExcMessage(
+                          "This type of triangulation is not supported!"));
+          }
+
+        ConstructionData<dim, spacedim> cd;
+
+        auto &cells             = cd.cells;
+        auto &vertices          = cd.vertices;
+        auto &boundary_ids      = cd.boundary_ids;
+        auto &coarse_lid_to_gid = cd.coarse_lid_to_gid;
+        auto &parts             = cd.parts;
+
+        auto add_vertices_of_cell_to_vertices_owned_by_loclly_owned_cells =
+          [](auto &cell, auto &vertices_owned_by_loclly_owned_cells) mutable {
+            for (unsigned int i = 0; i < GeometryInfo<dim>::faces_per_cell; i++)
+              if (cell->has_periodic_neighbor(i))
+                {
+                  auto face_t = cell->face(i);
+                  auto face_n = cell->periodic_neighbor(i)->face(
+                    cell->periodic_neighbor_face_no(i));
+                  for (unsigned int j = 0;
+                       j < GeometryInfo<dim>::vertices_per_face;
+                       j++)
+                    {
+                      vertices_owned_by_loclly_owned_cells.insert(
+                        face_t->vertex_index(j));
+                      vertices_owned_by_loclly_owned_cells.insert(
+                        face_n->vertex_index(j));
+                    }
+                }
+
+
+            for (unsigned int v = 0; v < GeometryInfo<dim>::vertices_per_cell;
+                 v++)
+              vertices_owned_by_loclly_owned_cells.insert(
+                cell->vertex_index(v));
+          };
+
+        if (!tria_pft.is_multilevel_hierarchy_constructed())
+          {
+            // 2) collect vertices of active locally owned cells
+            std::set<unsigned int> vertices_owned_by_loclly_owned_cells;
+            for (auto cell : tria.cell_iterators())
+              if (cell->active() && cell->subdomain_id() == my_rank)
+                add_vertices_of_cell_to_vertices_owned_by_loclly_owned_cells(
+                  cell, vertices_owned_by_loclly_owned_cells);
+
+            // helper function to determine if cell is locally relevant
+            auto is_locally_relevant = [&](auto &cell) {
+              for (unsigned int v = 0; v < GeometryInfo<dim>::vertices_per_cell;
+                   v++)
+                if (vertices_owned_by_loclly_owned_cells.find(
+                      cell->vertex_index(v)) !=
+                    vertices_owned_by_loclly_owned_cells.end())
+                  return true;
+              return false;
+            };
+
+            // 3) process all local and ghost cells: setup needed data
+            // structures and
+            //    collect all locally relevant vertices for second sweep
+            std::map<unsigned int, unsigned int> vertices_locally_relevant;
+            parts.push_back(Part());
+            Part &part = parts[0];
+
+            unsigned int cell_counter = 0;
+            for (auto cell : tria.cell_iterators())
+              if (cell->active() && is_locally_relevant(cell))
+                {
+                  // a) extract cell definition (with old numbering of
+                  // vertices)
+                  CellData<dim> cell_data;
+                  cell_data.material_id = cell->material_id();
+                  cell_data.manifold_id = cell->manifold_id();
+                  for (unsigned int v = 0;
+                       v < GeometryInfo<dim>::vertices_per_cell;
+                       v++)
+                    cell_data.vertices[v] = cell->vertex_index(v);
+                  cells.push_back(cell_data);
+
+                  // b) save indices of each vertex of this cell
+                  for (unsigned int v = 0;
+                       v < GeometryInfo<dim>::vertices_per_cell;
+                       v++)
+                    vertices_locally_relevant[cell->vertex_index(v)] =
+                      numbers::invalid_unsigned_int;
+
+                  // c) save boundary_ids of each face of this cell
+                  for (unsigned int f = 0;
+                       f < GeometryInfo<dim>::faces_per_cell;
+                       f++)
+                    boundary_ids.push_back(cell->face(f)->boundary_id());
+
+                  // e) save translation for corase grid: lid -> gid
+                  coarse_lid_to_gid[cell_counter] = convert_binary_to_gid<dim>(
+                    cell->id().template to_binary<dim>());
+
+                  CellId::binary_type id;
+                  id.fill(0);
+                  id[0] = cell_counter;
+                  id[1] = dim;
+                  id[2] = 0;
+                  id[3] = 0;
+
+                  part.cells.emplace_back(id,
+                                          cell->subdomain_id(),
+                                          numbers::invalid_subdomain_id);
+
+                  cell_counter++;
+                }
+
+            std::sort(part.cells.begin(), part.cells.end(), [](auto a, auto b) {
+              return convert_binary_to_gid<dim>(a.index) <
+                     convert_binary_to_gid<dim>(b.index);
+            });
+
+            // 4) enumerate locally relevant
+            unsigned int vertex_counter = 0;
+            for (auto &vertex : vertices_locally_relevant)
+              {
+                vertices.push_back(tria.get_vertices()[vertex.first]);
+                vertex.second = vertex_counter++;
+              }
+
+            // 5) correct vertices of cells (make them local)
+            for (auto &cell : cells)
+              for (unsigned int v = 0; v < GeometryInfo<dim>::vertices_per_cell;
+                   v++)
+                cell.vertices[v] = vertices_locally_relevant[cell.vertices[v]];
+          }
+        else
+          {
+            for (auto cell : tria.cell_iterators_on_level(0))
+              cell->recursively_clear_user_flag();
+
+            for (unsigned int level =
+                   tria.get_triangulation().n_global_levels() - 1;
+                 level != numbers::invalid_unsigned_int;
+                 level--)
+              {
+                std::set<unsigned int> vertices_owned_by_loclly_owned_cells;
+                for (auto cell : tria.cell_iterators_on_level(level))
+                  if (cell->level_subdomain_id() == my_rank ||
+                      (cell->active() && cell->subdomain_id() == my_rank))
+                    add_vertices_of_cell_to_vertices_owned_by_loclly_owned_cells(
+                      cell, vertices_owned_by_loclly_owned_cells);
+
+                for (auto cell : tria.active_cell_iterators())
+                  if (cell->subdomain_id() == my_rank)
+                    add_vertices_of_cell_to_vertices_owned_by_loclly_owned_cells(
+                      cell, vertices_owned_by_loclly_owned_cells);
+
+                // helper function to determine if cell is locally relevant
+                auto is_locally_relevant = [&](auto &cell) {
+                  // for(unsigned int v = 0; v <
+                  // GeometryInfo<dim>::vertices_per_cell; v++)
+                  //    std::cout << cell->vertex_index(v) << " ";
+                  // std::cout << std::endl;
+
+                  for (unsigned int v = 0;
+                       v < GeometryInfo<dim>::vertices_per_cell;
+                       v++)
+                    if (vertices_owned_by_loclly_owned_cells.find(
+                          cell->vertex_index(v)) !=
+                        vertices_owned_by_loclly_owned_cells.end())
+                      return true;
+                  return false;
+                };
+
+                // std::cout << "S" <<
+                // vertices_owned_by_loclly_owned_cells.size() << std::endl;
+
+                for (auto cell : tria.cell_iterators_on_level(level))
+                  if (is_locally_relevant(cell))
+                    set_flag_reverse(cell);
+              }
+
+
+
+            // 2) collect vertices of cells on level 0
+            std::map<unsigned int, unsigned int> vertices_locally_relevant;
+
+            unsigned int cell_counter = 0;
+            for (auto cell : tria.cell_iterators_on_level(0))
+              {
+                if (!cell->user_flag_set())
+                  continue;
+
+                // b) extract cell definition (with old numbering of vertices)
+                CellData<dim> cell_data;
+                cell_data.material_id = cell->material_id();
+                cell_data.manifold_id = cell->manifold_id();
+                for (unsigned int v = 0;
+                     v < GeometryInfo<dim>::vertices_per_cell;
+                     v++)
+                  cell_data.vertices[v] = cell->vertex_index(v);
+                cells.push_back(cell_data);
+
+                // c) save indices of each vertex of this cell
+                for (unsigned int v = 0;
+                     v < GeometryInfo<dim>::vertices_per_cell;
+                     v++)
+                  vertices_locally_relevant[cell->vertex_index(v)] =
+                    numbers::invalid_unsigned_int;
+
+                // d) save boundary_ids of each face of this cell
+                for (unsigned int f = 0; f < GeometryInfo<dim>::faces_per_cell;
+                     f++)
+                  boundary_ids.push_back(cell->face(f)->boundary_id());
+
+                // e) save translation for corase grid: lid -> gid
+                coarse_lid_to_gid[cell_counter] = convert_binary_to_gid<dim>(
+                  cell->id().template to_binary<dim>());
+
+                cell_counter++;
+              }
+
+            // 4) enumerate locally relevant
+            unsigned int vertex_counter = 0;
+            for (auto &vertex : vertices_locally_relevant)
+              {
+                vertices.push_back(tria.get_vertices()[vertex.first]);
+                vertex.second = vertex_counter++;
+              }
+
+            // 5) correct vertices of cells (make them local)
+            for (auto &cell : cells)
+              for (unsigned int v = 0; v < GeometryInfo<dim>::vertices_per_cell;
+                   v++)
+                cell.vertices[v] = vertices_locally_relevant[cell.vertices[v]];
+
+
+            std::map<int, int> coarse_gid_to_lid;
+            for (auto i : coarse_lid_to_gid)
+              coarse_gid_to_lid[i.second] = i.first;
+
+            for (unsigned int level = 0;
+                 level < tria.get_triangulation().n_global_levels();
+                 level++)
+              {
+                std::set<unsigned int> vertices_owned_by_loclly_owned_cells;
+                for (auto cell : tria.cell_iterators_on_level(level))
+                  if (cell->level_subdomain_id() == my_rank ||
+                      (cell->active() && cell->subdomain_id() == my_rank))
+                    add_vertices_of_cell_to_vertices_owned_by_loclly_owned_cells(
+                      cell, vertices_owned_by_loclly_owned_cells);
+
+                //      //if(level > 0)
+                //        for(auto cell : tria.active_cell_iterators())
+                //          if(cell->subdomain_id() == my_rank)
+                //            add_vertices_of_cell_to_vertices_owned_by_loclly_owned_cells(
+                //              cell, vertices_owned_by_loclly_owned_cells);
+
+                // helper function to determine if cell is locally relevant
+                auto is_locally_relevant = [&](auto &cell) {
+                  for (unsigned int v = 0;
+                       v < GeometryInfo<dim>::vertices_per_cell;
+                       v++)
+                    if (vertices_owned_by_loclly_owned_cells.find(
+                          cell->vertex_index(v)) !=
+                        vertices_owned_by_loclly_owned_cells.end())
+                      return true;
+                  return false;
+                };
+
+
+                std::set<unsigned int>
+                  vertices_owned_by_loclly_owned_cells_strong;
+                for (auto cell : tria.active_cell_iterators())
+                  if (cell->subdomain_id() == my_rank)
+                    add_vertices_of_cell_to_vertices_owned_by_loclly_owned_cells(
+                      cell, vertices_owned_by_loclly_owned_cells_strong);
+
+                // helper function to determine if cell is locally relevant
+                auto is_locally_relevant_strong = [&](auto &cell) {
+                  for (unsigned int v = 0;
+                       v < GeometryInfo<dim>::vertices_per_cell;
+                       v++)
+                    if (vertices_owned_by_loclly_owned_cells_strong.find(
+                          cell->vertex_index(v)) !=
+                        vertices_owned_by_loclly_owned_cells_strong.end())
+                      return true;
+                  return false;
+                };
+
+
+                parts.push_back(Part());
+                Part &part = parts.back();
+                for (auto cell : tria.cell_iterators_on_level(level))
+                  {
+                    if (!(cell->user_flag_set()))
+                      continue;
+
+                    auto id = cell->id().template to_binary<dim>();
+                    id[0]   = coarse_gid_to_lid[id[0]];
+
+                    if (cell->active() && is_locally_relevant_strong(cell))
+                      part.cells.emplace_back(id,
+                                              cell->subdomain_id(),
+                                              cell->level_subdomain_id());
+                    else if (is_locally_relevant(cell))
+                      part.cells.emplace_back(id,
+                                              numbers::artificial_subdomain_id,
+                                              cell->level_subdomain_id());
+                    else
+                      part.cells.emplace_back(id,
+                                              numbers::artificial_subdomain_id,
+                                              numbers::artificial_subdomain_id);
+                  }
+
+                std::sort(part.cells.begin(),
+                          part.cells.end(),
+                          [](auto a, auto b) {
+                            return convert_binary_to_gid<dim>(a.index) <
+                                   convert_binary_to_gid<dim>(b.index);
+                          });
+
+
+                // for(auto cell : part.cells)
+                //    std::cout << CellId(cell.index).to_string() <<
+                //    std::endl;
+              }
+          }
+
+        return cd;
+      }
+    } // namespace Utilities
+  }   // namespace fullydistributed
+} // namespace parallel
+
+
+DEAL_II_NAMESPACE_CLOSE
+
+#endif
