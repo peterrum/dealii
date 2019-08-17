4b5999d3bee4f288ae0c9eaf6a50566eb080e133
Peter Munch <peterrmuench@gmail.com>
2019-08-17 14:48:25 +0200

    Add p:f:t

---
+++ include/deal.II/distributed/fullydistributed_tria.h
@@ -0,0 +1,247 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2008 - 2019 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+#ifndef dealii_fullydistributed_tria_h
+#define dealii_fullydistributed_tria_h
+
+
+#include <deal.II/base/config.h>
+
+#include <deal.II/base/smartpointer.h>
+#include <deal.II/base/subscriptor.h>
+#include <deal.II/base/template_constraints.h>
+#include <deal.II/base/timer.h>
+
+#include <deal.II/distributed/p4est_wrappers.h>
+#include <deal.II/distributed/tria_base.h>
+
+#include <deal.II/grid/tria.h>
+
+#include <boost/range/iterator_range.hpp>
+
+#include <functional>
+#include <list>
+#include <set>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+#ifdef DEAL_II_WITH_MPI
+#  include <mpi.h>
+#endif
+
+#ifdef DEAL_II_WITH_P4EST
+#  include <p4est.h>
+#  include <p4est_connectivity.h>
+#  include <p4est_ghost.h>
+#  include <p8est.h>
+#  include <p8est_connectivity.h>
+#  include <p8est_ghost.h>
+#endif
+
+DEAL_II_NAMESPACE_OPEN
+
+namespace internal
+{
+  namespace DoFHandlerImplementation
+  {
+    namespace Policy
+    {
+      template <typename>
+      class ParallelDistributed;
+    } // namespace Policy
+  }   // namespace DoFHandlerImplementation
+} // namespace internal
+
+namespace FETools
+{
+  namespace internal
+  {
+    template <int, int, class>
+    class ExtrapolateImplementation;
+  }
+} // namespace FETools
+
+// forward declaration of the data type for periodic face pairs
+namespace GridTools
+{
+  template <typename CellIterator>
+  struct PeriodicFacePair;
+}
+
+struct Part_
+{
+  Part_()
+  {}
+
+  Part_(CellId::binary_type index,
+        unsigned int        subdomain_id,
+        unsigned int        level_subdomain_id)
+    : index(index)
+    , subdomain_id(subdomain_id)
+    , level_subdomain_id(level_subdomain_id){};
+
+  CellId::binary_type index;
+  unsigned int        subdomain_id;
+  unsigned int        level_subdomain_id;
+};
+
+class Part
+{
+public:
+  std::vector<Part_> cells;
+};
+
+namespace parallel
+{
+  namespace fullydistributed
+  {
+    template <int dim, int spacedim>
+    struct ConstructionData
+    {
+      // information describing the local part of the coarse grid
+      std::vector<CellData<dim>>      cells;
+      std::vector<Point<spacedim>>    vertices;
+      std::vector<types::boundary_id> boundary_ids;
+
+      // information
+      std::map<int, int> coarse_lid_to_gid;
+
+      // information describing how to constuct the levels
+      std::vector<Part> parts;
+    };
+
+    template <int dim, int spacedim = dim>
+    class Triangulation
+      : public parallel::DistributedTrinagulationBase<dim, spacedim>
+    {
+    public:
+      typedef typename dealii::Triangulation<dim, spacedim>::cell_iterator
+        cell_iterator;
+
+      typedef
+        typename dealii::Triangulation<dim, spacedim>::active_cell_iterator
+          active_cell_iterator;
+
+      typedef
+        typename dealii::Triangulation<dim, spacedim>::CellStatus CellStatus;
+
+      void
+      reinit(ConstructionData<dim, spacedim> &construction_data);
+
+      virtual void
+      create_triangulation(const std::vector<Point<spacedim>> &vertices,
+                           const std::vector<CellData<dim>> &  cells,
+                           const SubCellData &subcelldata) override;
+
+
+      enum Settings
+      {
+        /**
+         * Default settings, other options are disabled.
+         */
+        default_setting = 0x0,
+        /**
+         * This flags needs to be set to use the geometric multigrid
+         * functionality. This option requires additional computation and
+         * communication. Note: geometric multigrid is still a work in
+         * progress.
+         */
+        construct_multigrid_hierarchy = 0x1
+      };
+
+
+      Triangulation(MPI_Comm mpi_communicator,
+                    Settings settings_ = default_setting);
+
+      Triangulation(MPI_Comm mpi_communicator,
+                    MPI_Comm mpi_communicator_coarse,
+                    Settings settings_ = default_setting);
+
+      virtual ~Triangulation();
+
+      virtual void
+      clear() override;
+
+      void
+      copy_local_forest_to_triangulation();
+
+      virtual void
+      execute_coarsening_and_refinement() override;
+
+      virtual bool
+      prepare_coarsening_and_refinement() override;
+
+      virtual bool
+      has_hanging_nodes() const override;
+
+      virtual std::size_t
+      memory_consumption() const override;
+
+      virtual std::size_t
+      memory_consumption_p4est() const;
+
+      virtual void
+      add_periodicity(
+        const std::vector<GridTools::PeriodicFacePair<cell_iterator>> &)
+        override;
+
+      virtual void
+      update_number_cache() override;
+
+      virtual std::map<unsigned int, std::set<dealii::types::subdomain_id>>
+      compute_vertices_with_ghost_neighbors() const override;
+
+      bool
+      do_construct_multigrid_hierarchy() const override;
+
+      MPI_Comm
+      get_coarse_communicator() const;
+
+      virtual unsigned int
+      translate_coarse_cell_id_to_coarse_cell_index(
+        const unsigned int coarse_cell_id) const override;
+
+      virtual unsigned int
+      translate_coarse_cell_index_to_coarse_cell_id(
+        const unsigned int coarse_cell_index) const override;
+
+    private:
+      /**
+       * store the Settings.
+       */
+      Settings settings;
+
+      template <typename>
+      friend class dealii::internal::DoFHandlerImplementation::Policy::
+        ParallelDistributed;
+
+      template <int, int, class>
+      friend class dealii::FETools::internal::ExtrapolateImplementation;
+
+      std::map<int, int> coarse_gid_to_lid;
+      std::map<int, int> coarse_lid_to_gid;
+
+      MPI_Comm mpi_communicator_coarse;
+    };
+
+  } // namespace fullydistributed
+} // namespace parallel
+
+
+DEAL_II_NAMESPACE_CLOSE
+
+#endif
---
+++ source/distributed/fullydistributed_tria.cc
@@ -0,0 +1,591 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2008 - 2019 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+
+#include <deal.II/base/logstream.h>
+#include <deal.II/base/memory_consumption.h>
+#include <deal.II/base/utilities.h>
+
+#include <deal.II/distributed/fullydistributed_tria.h>
+#include <deal.II/distributed/p4est_wrappers.h>
+
+#include <deal.II/grid/grid_tools.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/tria_accessor.h>
+#include <deal.II/grid/tria_iterator.h>
+
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/sparsity_tools.h>
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+
+DEAL_II_NAMESPACE_OPEN
+
+namespace parallel
+{
+  namespace fullydistributed
+  {
+    namespace internal
+    {
+      template <int dim, typename T>
+      bool
+      parent(T c, T p)
+      {
+        if (c[0] != p[0])
+          return false; // has same coarse cell
+        if ((c[1] >> 2) != ((p[1] >> 2) + 1))
+          return false; // level has one difference
+
+
+        const unsigned int n_child_indices = p[1] >> 2;
+        const unsigned int children_per_value =
+          sizeof(CellId::binary_type::value_type) * 8 / dim;
+        unsigned int child_level  = 0;
+        unsigned int binary_entry = 2;
+
+        // Loop until all child indices have been written
+        while (child_level < n_child_indices)
+          {
+            Assert(binary_entry < c.size(), ExcInternalError());
+
+            for (unsigned int j = 0; j < children_per_value; ++j)
+              {
+                // const unsigned int child_index =
+                //  static_cast<unsigned int>(child_indices[child_level]);
+                // Shift the child index to its position in the unsigned int and
+                // store it
+                if ((((c[binary_entry] >> (j * dim))) &
+                     (GeometryInfo<dim>::max_children_per_cell - 1)) !=
+                    (((p[binary_entry] >> (j * dim))) &
+                     (GeometryInfo<dim>::max_children_per_cell - 1)))
+                  return false;
+
+
+                ++child_level;
+                if (child_level == n_child_indices)
+                  break;
+              }
+
+            ++binary_entry;
+          }
+
+        //            if(( *((long int*)(&c[2])) << (64 - dim* c[1])) != (
+        //            *((long int*)(&p[2])) << (64 - dim* c[1])) )
+        //                return false;
+        //
+        return true;
+      }
+    } // namespace internal
+
+    template <int dim, int spacedim>
+    void
+    Triangulation<dim, spacedim>::reinit(
+      ConstructionData<dim, spacedim> &construction_data)
+    {
+      if (construction_data.cells.size() == 0)
+        {
+          Point<dim> p_1, p_2;
+          for (unsigned int i = 0; i < dim; ++i)
+            {
+              p_1(i) = 0;
+              p_2(i) = 1;
+            }
+
+
+          Point<spacedim> p1, p2;
+          for (unsigned int i = 0; i < dim; ++i)
+            {
+              p1(i) = std::min(p_1(i), p_2(i));
+              p2(i) = std::max(p_1(i), p_2(i));
+            }
+
+          std::vector<Point<spacedim>> vertices(
+            GeometryInfo<dim>::vertices_per_cell);
+          switch (dim)
+            {
+              case 1:
+                vertices[0] = p1;
+                vertices[1] = p2;
+                break;
+              case 2:
+                vertices[0] = vertices[1] = p1;
+                vertices[2] = vertices[3] = p2;
+
+                vertices[1](0) = p2(0);
+                vertices[2](0) = p1(0);
+                break;
+              case 3:
+                vertices[0] = vertices[1] = vertices[2] = vertices[3] = p1;
+                vertices[4] = vertices[5] = vertices[6] = vertices[7] = p2;
+
+                vertices[1](0) = p2(0);
+                vertices[2](1) = p2(1);
+                vertices[3](0) = p2(0);
+                vertices[3](1) = p2(1);
+
+                vertices[4](0) = p1(0);
+                vertices[4](1) = p1(1);
+                vertices[5](1) = p1(1);
+                vertices[6](0) = p1(0);
+
+
+                break;
+              default:
+                Assert(false, ExcNotImplemented());
+            }
+          // Prepare cell data
+          std::vector<CellData<dim>> cells(1);
+          for (unsigned int i = 0; i < GeometryInfo<dim>::vertices_per_cell;
+               ++i)
+            cells[0].vertices[i] = i;
+          cells[0].material_id = 0;
+          SubCellData subcelldata;
+          dealii::parallel::Triangulation<dim, spacedim>::create_triangulation(
+            vertices, cells, subcelldata);
+
+          for (auto cell = this->begin(); cell != this->end(); cell++)
+            {
+              cell->set_subdomain_id(dealii::numbers::artificial_subdomain_id);
+              cell->set_level_subdomain_id(
+                dealii::numbers::artificial_subdomain_id);
+            }
+
+          this->coarse_gid_to_lid.emplace(0, 0);
+          this->coarse_lid_to_gid.emplace(0, 0);
+
+          update_number_cache();
+          return;
+        }
+
+      auto &boundary_ids = construction_data.boundary_ids;
+      auto &cells        = construction_data.cells;
+      auto &vertices     = construction_data.vertices;
+      auto &parts        = construction_data.parts;
+
+
+      // save data structures
+      this->coarse_lid_to_gid = construction_data.coarse_lid_to_gid;
+
+      std::map<int, int> coarse_gid_to_lid;
+      for (auto &i : coarse_lid_to_gid)
+        coarse_gid_to_lid[i.second] = i.first;
+      this->coarse_gid_to_lid = coarse_gid_to_lid;
+
+
+
+      // 1) create coarse grid
+      SubCellData subcelldata;
+      dealii::parallel::Triangulation<dim, spacedim>::create_triangulation(
+        vertices, cells, subcelldata);
+
+      for (auto c = this->begin(); c != this->end(); c++)
+        c->set_all_manifold_ids(c->material_id());
+
+
+      // 2) set boundary ids
+      int c = 0;
+      for (auto cell = this->begin_active(); cell != this->end(); ++cell)
+        for (unsigned int i = 0; i < GeometryInfo<dim>::faces_per_cell; i++)
+          {
+            unsigned int boundary_ind = boundary_ids[c++];
+            if (boundary_ind != numbers::internal_face_boundary_id)
+              cell->face(i)->set_boundary_id(boundary_ind);
+          }
+
+      // 3) create all cell levels
+      for (unsigned int ref_counter = 1; ref_counter < parts.size();
+           ref_counter++)
+        {
+          auto coarse_cell    = this->begin(ref_counter - 1);
+          auto fine_cell_info = parts[ref_counter].cells.begin();
+
+          for (; fine_cell_info != parts[ref_counter].cells.end();
+               ++fine_cell_info)
+            {
+              while (!internal::parent<dim>(
+                fine_cell_info->index,
+                coarse_cell->id().template to_binary<dim>()))
+                coarse_cell++;
+
+              coarse_cell->set_refine_flag();
+            }
+
+          dealii::Triangulation<dim,
+                                spacedim>::execute_coarsening_and_refinement();
+        }
+
+
+
+      // 4a) set all cells artificial
+      for (auto cell = this->begin(); cell != this->end(); cell++)
+        {
+          if (cell->active())
+            cell->set_subdomain_id(dealii::numbers::artificial_subdomain_id);
+
+          if (settings & construct_multigrid_hierarchy)
+            cell->set_level_subdomain_id(
+              dealii::numbers::artificial_subdomain_id);
+        }
+
+
+
+      // 4b) set actual (level_)subdomain_ids
+      for (unsigned int ref_counter = 0; ref_counter < parts.size();
+           ref_counter++)
+        {
+          auto cell      = this->begin(ref_counter);
+          auto cell_info = parts[ref_counter].cells.begin();
+          for (; cell_info != parts[ref_counter].cells.end(); ++cell_info)
+            {
+              auto temp = cell_info->index;
+              temp[0] = translate_coarse_cell_index_to_coarse_cell_id(temp[0]);
+              while (temp != cell->id().template to_binary<dim>())
+                cell++;
+
+              if (cell->active())
+                cell->set_subdomain_id(cell_info->subdomain_id);
+
+              if (settings & construct_multigrid_hierarchy)
+                cell->set_level_subdomain_id(cell_info->level_subdomain_id);
+            }
+        }
+
+
+
+      // 5)
+      update_number_cache();
+    }
+
+
+
+    template <int dim, int spacedim>
+    void
+    Triangulation<dim, spacedim>::create_triangulation(
+      const std::vector<Point<spacedim>> &vertices,
+      const std::vector<CellData<dim>> &  cells,
+      const SubCellData &                 subcelldata)
+    {
+      std::cout
+        << "parallel::fullydistributed::Triangulation::create_triangulation()::start"
+        << std::endl;
+
+      dealii::parallel::Triangulation<dim, spacedim>::create_triangulation(
+        vertices, cells, subcelldata);
+
+      std::cout
+        << "parallel::fullydistributed::Triangulation::create_triangulation()::end"
+        << std::endl;
+    }
+
+
+
+    template <int dim, int spacedim>
+    Triangulation<dim, spacedim>::Triangulation(MPI_Comm mpi_communicaton,
+                                                Settings settings_)
+      : Triangulation<dim, spacedim>(mpi_communicaton,
+                                     mpi_communicaton,
+                                     settings_)
+    {}
+
+
+
+    template <int dim, int spacedim>
+    Triangulation<dim, spacedim>::Triangulation(
+      MPI_Comm mpi_communicator,
+      MPI_Comm mpi_communicator_coarse,
+      Settings settings_)
+      : parallel::DistributedTrinagulationBase<dim, spacedim>(
+          mpi_communicator,
+          (settings_ & construct_multigrid_hierarchy) ?
+            static_cast<
+              typename dealii::Triangulation<dim, spacedim>::MeshSmoothing>(
+              dealii::Triangulation<dim>::none |
+              Triangulation<dim,
+                            spacedim>::limit_level_difference_at_vertices) :
+            static_cast<
+              typename dealii::Triangulation<dim, spacedim>::MeshSmoothing>(
+              dealii::Triangulation<dim>::none),
+          false)
+      , settings(settings_)
+      , mpi_communicator_coarse(mpi_communicator_coarse)
+    {}
+
+
+
+    template <int dim, int spacedim>
+    Triangulation<dim, spacedim>::~Triangulation()
+    {}
+
+
+
+    template <int dim, int spacedim>
+    void
+    Triangulation<dim, spacedim>::clear()
+    {}
+
+
+
+    template <int dim, int spacedim>
+    void
+    Triangulation<dim, spacedim>::update_number_cache()
+    {
+      parallel::Triangulation<dim, spacedim>::update_number_cache();
+
+      if (settings & construct_multigrid_hierarchy)
+        parallel::Triangulation<dim, spacedim>::fill_level_ghost_owners();
+    }
+
+
+
+    template <int dim, int spacedim>
+    void
+    Triangulation<dim, spacedim>::copy_local_forest_to_triangulation()
+    {
+      Assert(false, ExcNotImplemented());
+    }
+
+
+
+    template <int dim, int spacedim>
+    void
+    Triangulation<dim, spacedim>::execute_coarsening_and_refinement()
+    {
+      Assert(false, ExcNotImplemented());
+    }
+
+
+
+    template <int dim, int spacedim>
+    bool
+    Triangulation<dim, spacedim>::prepare_coarsening_and_refinement()
+    {
+      Assert(false, ExcNotImplemented());
+      return false;
+    }
+
+
+
+    template <int dim, int spacedim>
+    bool
+    Triangulation<dim, spacedim>::has_hanging_nodes() const
+    {
+      Assert(false, ExcNotImplemented());
+      return false;
+    }
+
+
+
+    template <int dim, int spacedim>
+    std::size_t
+    Triangulation<dim, spacedim>::memory_consumption() const
+    {
+      Assert(false, ExcNotImplemented());
+      return 0;
+    }
+
+
+
+    template <int dim, int spacedim>
+    std::size_t
+    Triangulation<dim, spacedim>::memory_consumption_p4est() const
+    {
+      // TODO: this should be not accessed!
+      return 0;
+    }
+
+
+
+    template <int dim, int spacedim>
+    void
+    Triangulation<dim, spacedim>::add_periodicity(
+      const std::vector<GridTools::PeriodicFacePair<cell_iterator>>
+        &periodicity_vector)
+    {
+      dealii::Triangulation<dim, spacedim>::add_periodicity(periodicity_vector);
+    }
+
+
+
+    template <int dim, int spacedim>
+    std::map<unsigned int, std::set<dealii::types::subdomain_id>>
+    Triangulation<dim, spacedim>::compute_vertices_with_ghost_neighbors() const
+    {
+      std::vector<bool> vertex_of_own_cell(this->n_vertices(), false);
+
+      // collect nodes coinciding due to periodicity
+      std::map<unsigned int, unsigned int> periodic_map;
+      for (auto &cell : this->active_cell_iterators())
+        if (cell->is_locally_owned() || cell->is_ghost())
+          {
+            for (unsigned int i = 0; i < GeometryInfo<dim>::faces_per_cell; i++)
+              {
+                if (cell->has_periodic_neighbor(i) &&
+                    cell->periodic_neighbor(i)->active())
+                  {
+                    auto face_t = cell->face(i);
+                    auto face_n = cell->periodic_neighbor(i)->face(
+                      cell->periodic_neighbor_face_no(i));
+                    for (unsigned int j = 0;
+                         j < GeometryInfo<dim>::vertices_per_face;
+                         j++)
+                      {
+                        auto         v_t  = face_t->vertex_index(j);
+                        auto         v_n  = face_n->vertex_index(j);
+                        unsigned int temp = std::min(v_t, v_n);
+                        {
+                          auto it = periodic_map.find(v_t);
+                          if (it != periodic_map.end())
+                            temp = std::min(temp, it->second);
+                        }
+                        {
+                          auto it = periodic_map.find(v_n);
+                          if (it != periodic_map.end())
+                            temp = std::min(temp, it->second);
+                        }
+                        periodic_map[v_t] = temp;
+                        periodic_map[v_n] = temp;
+                      }
+                  }
+              }
+          }
+
+      // compress map
+      for (auto &p : periodic_map)
+        {
+          if (p.first == p.second)
+            continue;
+          unsigned int temp = p.second;
+          while (temp != periodic_map[temp])
+            temp = periodic_map[temp];
+          p.second = temp;
+        }
+
+      // check if map is actually compressed
+      for (auto p : periodic_map)
+        {
+          if (p.first == p.second)
+            continue;
+          auto pp = periodic_map.find(p.second);
+          if (pp->first == pp->second)
+            continue;
+          AssertThrow(false, ExcMessage("Map has to be compressed!"));
+        }
+
+      std::map<unsigned int, std::set<unsigned int>> sets;
+      for (auto p : periodic_map)
+        sets[p.second] = std::set<unsigned int>();
+
+      for (auto p : periodic_map)
+        sets[p.second].insert(p.first);
+
+      std::map<unsigned int, std::set<unsigned int>> sets2;
+      for (auto &s : sets)
+        {
+          for (auto &ss : s.second)
+            sets2[ss] = s.second;
+        }
+
+      for (const auto &cell : this->active_cell_iterators())
+        if (cell->is_locally_owned())
+          {
+            for (unsigned int v = 0; v < GeometryInfo<dim>::vertices_per_cell;
+                 ++v)
+              vertex_of_own_cell[cell->vertex_index(v)] = true;
+          }
+
+      std::map<unsigned int, std::set<dealii::types::subdomain_id>> result;
+      for (const auto &cell : this->active_cell_iterators())
+        if (cell->is_ghost())
+          {
+            const types::subdomain_id owner = cell->subdomain_id();
+            for (unsigned int v = 0; v < GeometryInfo<dim>::vertices_per_cell;
+                 ++v)
+              {
+                if (vertex_of_own_cell[cell->vertex_index(v)])
+                  result[cell->vertex_index(v)].insert(owner);
+
+                // mark also nodes coinciding due to periodicity
+                auto t = sets2.find(cell->vertex_index(v));
+                if (t != sets2.end())
+                  for (auto i : t->second)
+                    if (vertex_of_own_cell[i])
+                      result[i].insert(owner);
+              }
+          }
+
+      return result;
+    }
+
+
+
+    template <int dim, int spacedim>
+    bool
+    Triangulation<dim, spacedim>::do_construct_multigrid_hierarchy() const
+    {
+      return (settings & construct_multigrid_hierarchy);
+    }
+
+
+
+    template <int dim, int spacedim>
+    MPI_Comm
+    Triangulation<dim, spacedim>::get_coarse_communicator() const
+    {
+      return mpi_communicator_coarse;
+    }
+
+
+
+    template <int dim, int spacedim>
+    unsigned int
+    Triangulation<dim, spacedim>::translate_coarse_cell_id_to_coarse_cell_index(
+      const unsigned int coarse_cell_id) const
+    {
+      auto coarse_cell_index = coarse_gid_to_lid.find(coarse_cell_id);
+      Assert(coarse_cell_index != coarse_gid_to_lid.end(),
+             ExcMessage("Coarse cell index not found!"));
+      return coarse_cell_index->first;
+    }
+
+
+
+    template <int dim, int spacedim>
+    unsigned int
+    Triangulation<dim, spacedim>::translate_coarse_cell_index_to_coarse_cell_id(
+      const unsigned int coarse_cell_index) const
+    {
+      auto coarse_cell_id = coarse_lid_to_gid.find(coarse_cell_index);
+      Assert(coarse_cell_id != coarse_lid_to_gid.end(),
+             ExcMessage("Coarse cell id not found!"));
+      return coarse_cell_id->second;
+    }
+
+
+
+  } // namespace fullydistributed
+} // namespace parallel
+
+
+
+/*-------------- Explicit Instantiations -------------------------------*/
+#include "fullydistributed_tria.inst"
+
+
+DEAL_II_NAMESPACE_CLOSE
