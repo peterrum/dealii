ea119e80cc4a50bf413a5b991c9809f9410c239e
Peter Munch <peterrmuench@gmail.com>
2019-12-27 22:03:23 +0100

    Remove some template arguments

--- include/deal.II/multigrid/mg_transfer_interface.h
+++ include/deal.II/multigrid/mg_transfer_interface.h
@@ -20,6 +20,127 @@
 
 DEAL_II_NAMESPACE_OPEN
 
+class CellTransfer
+{
+public:
+  CellTransfer(const unsigned int degree_fine, const unsigned int degree_coarse)
+    : n(100 * degree_fine + degree_coarse)
+  {}
+
+  template <typename Fu>
+  void
+  run(Fu &fu)
+  {
+    if (do_run_degree<1>(fu))
+      return;
+    if (do_run_degree<2>(fu))
+      return;
+    Assert(false, ExcNotInitialized());
+  }
+
+  template <int degree, typename Fu>
+  bool
+  do_run_degree(Fu &fu)
+  {
+    if (n == 100 * degree + std::min(degree / 2, 1))
+      {
+        fu.template run<degree, std::min(degree / 2, 1)>();
+        return true;
+      }
+
+    if (n == 100 * (2 * degree + 1) + degree)
+      {
+        fu.template run<2 * degree + 1, degree>();
+        return true;
+      }
+
+    if (n == 100 * degree + std::min(degree - 1, 1))
+      {
+        fu.template run<degree, std::min(degree - 1, 1)>();
+        return true;
+      }
+
+    if (n == 100 * degree + 1)
+      {
+        fu.template run<degree, 1>();
+        return true;
+      }
+
+    return false;
+  }
+
+  const unsigned int n;
+};
+
+template <int dim, typename Number>
+class CellProlongator
+{
+public:
+  CellProlongator(const AlignedVector<Number> &prolongation_matrix_1d,
+                  const Number *               evaluation_data_coarse,
+                  Number *                     evaluation_data_fine)
+    : prolongation_matrix_1d(prolongation_matrix_1d)
+    , evaluation_data_coarse(evaluation_data_coarse)
+    , evaluation_data_fine(evaluation_data_fine)
+  {}
+
+  template <int degree_fine, int degree_coarse>
+  void
+  run()
+  {
+    internal::FEEvaluationImplBasisChange<
+      internal::evaluate_general,
+      dim,
+      degree_coarse + 1,
+      degree_fine + 1,
+      1,
+      Number,
+      Number>::do_forward(prolongation_matrix_1d,
+                          evaluation_data_coarse,
+                          evaluation_data_fine);
+  }
+
+private:
+  const AlignedVector<Number> &prolongation_matrix_1d;
+  const Number *               evaluation_data_coarse;
+  Number *                     evaluation_data_fine;
+};
+
+template <int dim, typename Number>
+class CellRestrictor
+{
+public:
+  CellRestrictor(const AlignedVector<Number> &prolongation_matrix_1d,
+                 Number *                     evaluation_data_fine,
+                 Number *                     evaluation_data_coarse)
+    : prolongation_matrix_1d(prolongation_matrix_1d)
+    , evaluation_data_fine(evaluation_data_fine)
+    , evaluation_data_coarse(evaluation_data_coarse)
+  {}
+
+  template <int degree_fine, int degree_coarse>
+  void
+  run()
+  {
+    internal::FEEvaluationImplBasisChange<
+      internal::evaluate_general,
+      dim,
+      degree_coarse + 1,
+      degree_fine + 1,
+      1,
+      Number,
+      Number>::do_backward(prolongation_matrix_1d,
+                           false,
+                           evaluation_data_fine,
+                           evaluation_data_coarse);
+  }
+
+private:
+  const AlignedVector<Number> &prolongation_matrix_1d;
+  Number *                     evaluation_data_fine;
+  Number *                     evaluation_data_coarse;
+};
+
 template <typename Number>
 struct TransferScheme
 {
@@ -39,6 +160,8 @@ struct TransferScheme
 
   std::vector<unsigned int> level_dof_indices_coarse;
   std::vector<unsigned int> level_dof_indices_fine;
+
+
 
   void
   print() const
@@ -78,7 +201,6 @@ public:
   }
 
 protected:
-  template <int degree_fine, int degree_coarse>
   void
   do_prolongate_add(const unsigned int                                to_level,
                     LinearAlgebra::distributed::Vector<Number> &      dst,
@@ -103,6 +225,12 @@ protected:
         AlignedVector<VectorizedArray<Number>> evaluation_data_coarse(
           scheme.n_cell_dofs_fine);
 
+        CellProlongator<dim, VectorizedArray<Number>> cell_prolongator(
+          scheme.prolongation_matrix_1d,
+          evaluation_data_coarse.begin(),
+          evaluation_data_fine.begin());
+        CellTransfer cell_transfer(scheme.degree_fine, scheme.degree_coarse);
+
         for (unsigned int cell = 0; cell < scheme.n_cells_coarse;
              cell += vec_size)
           {
@@ -126,38 +254,7 @@ protected:
             }
 
             // ---------------------------- coarse -----------------------------
-            if (scheme.degree_coarse == degree_coarse &&
-                scheme.degree_fine == degree_coarse)
-              {
-                internal::FEEvaluationImplBasisChange<
-                  internal::evaluate_general,
-                  dim,
-                  degree_coarse + 1,
-                  degree_coarse + 1,
-                  1,
-                  VectorizedArray<Number>,
-                  VectorizedArray<Number>>::
-                  do_forward(scheme.prolongation_matrix_1d,
-                             evaluation_data_coarse.begin(),
-                             evaluation_data_fine.begin());
-              }
-            else if (scheme.degree_coarse == degree_coarse &&
-                     scheme.degree_fine == degree_fine)
-              {
-                internal::FEEvaluationImplBasisChange<
-                  internal::evaluate_general,
-                  dim,
-                  degree_coarse + 1,
-                  degree_fine + 1,
-                  1,
-                  VectorizedArray<Number>,
-                  VectorizedArray<Number>>::
-                  do_forward(scheme.prolongation_matrix_1d,
-                             evaluation_data_coarse.begin(),
-                             evaluation_data_fine.begin());
-              }
-            else
-              Assert(false, ExcNotInitialized());
+            cell_transfer.run(cell_prolongator);
             // ------------------------------ fine -----------------------------
 
             if (scheme.fine_element_is_continuous)
@@ -197,7 +294,6 @@ protected:
     dst.copy_locally_owned_data_from(this->vec_fine);
   }
 
-  template <int degree_fine, int degree_coarse>
   void
   do_restrict_add(const unsigned int                                from_level,
                   LinearAlgebra::distributed::Vector<Number> &      dst,
@@ -218,6 +314,12 @@ protected:
           scheme.n_cell_dofs_fine);
         AlignedVector<VectorizedArray<Number>> evaluation_data_coarse(
           scheme.n_cell_dofs_fine);
+
+        CellRestrictor<dim, VectorizedArray<Number>> cell_restrictor(
+          scheme.prolongation_matrix_1d,
+          evaluation_data_fine.begin(),
+          evaluation_data_coarse.begin());
+        CellTransfer cell_transfer(scheme.degree_fine, scheme.degree_coarse);
 
         for (unsigned int cell = 0; cell < scheme.n_cells_coarse;
              cell += vec_size)
@@ -253,40 +355,7 @@ protected:
               }
 
             // ------------------------------ fine -----------------------------
-            if (scheme.degree_coarse == degree_coarse &&
-                scheme.degree_fine == degree_coarse)
-              {
-                internal::FEEvaluationImplBasisChange<
-                  internal::evaluate_general,
-                  dim,
-                  degree_coarse + 1,
-                  degree_coarse + 1,
-                  1,
-                  VectorizedArray<Number>,
-                  VectorizedArray<Number>>::
-                  do_backward(scheme.prolongation_matrix_1d,
-                              false,
-                              evaluation_data_fine.begin(),
-                              evaluation_data_coarse.begin());
-              }
-            else if (scheme.degree_coarse == degree_coarse &&
-                     scheme.degree_fine == degree_fine)
-              {
-                internal::FEEvaluationImplBasisChange<
-                  internal::evaluate_general,
-                  dim,
-                  degree_coarse + 1,
-                  degree_fine + 1,
-                  1,
-                  VectorizedArray<Number>,
-                  VectorizedArray<Number>>::
-                  do_backward(scheme.prolongation_matrix_1d,
-                              false,
-                              evaluation_data_fine.begin(),
-                              evaluation_data_coarse.begin());
-              }
-            else
-              Assert(false, ExcNotInitialized());
+            cell_transfer.run(cell_restrictor);
             // ----------------------------- coarse ----------------------------
 
 
