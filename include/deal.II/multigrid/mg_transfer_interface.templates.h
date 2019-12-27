a16c888966391e12484201f1ab4cfa315d21c5e0
Peter Munch <peterrmuench@gmail.com>
2019-12-27 22:36:09 +0100

    Move code into .cc-file and instantiate to degree 9

---
+++ include/deal.II/multigrid/mg_transfer_interface.templates.h
@@ -0,0 +1,383 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2003 - 2019 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+
+#ifndef dealii_mg_transfer_inteface_templates_h
+#define dealii_mg_transfer_inteface_templates_h
+
+#include <deal.II/base/config.h>
+
+#include <deal.II/matrix_free/evaluation_kernels.h>
+
+#include <deal.II/multigrid/mg_transfer_interface.h>
+
+DEAL_II_NAMESPACE_OPEN
+
+
+class CellTransfer
+{
+public:
+  CellTransfer(const unsigned int degree_fine, const unsigned int degree_coarse)
+    : n(100 * degree_fine + degree_coarse)
+  {}
+
+  template <typename Fu>
+  void
+  run(Fu &fu)
+  {
+    if (do_run_degree<1>(fu))
+      return;
+    if (do_run_degree<2>(fu))
+      return;
+    if (do_run_degree<3>(fu))
+      return;
+    if (do_run_degree<4>(fu))
+      return;
+    if (do_run_degree<5>(fu))
+      return;
+    if (do_run_degree<6>(fu))
+      return;
+    if (do_run_degree<7>(fu))
+      return;
+    if (do_run_degree<8>(fu))
+      return;
+    if (do_run_degree<9>(fu))
+      return;
+    Assert(false,
+           ExcMessage("Transfer " + std::to_string(n) + " not instantiated!"));
+  }
+
+  template <int degree, typename Fu>
+  bool
+  do_run_degree(Fu &fu)
+  {
+    if (n == 100 * degree + std::max(degree / 2, 1))
+      {
+        fu.template run<degree, std::max(degree / 2, 1)>();
+        return true;
+      }
+
+    if (n == 100 * (2 * degree + 1) + degree)
+      {
+        fu.template run<2 * degree + 1, degree>();
+        return true;
+      }
+
+    if (n == 100 * degree + std::max(degree - 1, 1))
+      {
+        fu.template run<degree, std::max(degree - 1, 1)>();
+        return true;
+      }
+
+    if (n == 100 * degree + 1)
+      {
+        fu.template run<degree, 1>();
+        return true;
+      }
+
+    return false;
+  }
+
+  const int n;
+};
+
+template <int dim, typename Number>
+class CellProlongator
+{
+public:
+  CellProlongator(const AlignedVector<Number> &prolongation_matrix_1d,
+                  const Number *               evaluation_data_coarse,
+                  Number *                     evaluation_data_fine)
+    : prolongation_matrix_1d(prolongation_matrix_1d)
+    , evaluation_data_coarse(evaluation_data_coarse)
+    , evaluation_data_fine(evaluation_data_fine)
+  {}
+
+  template <int degree_fine, int degree_coarse>
+  void
+  run()
+  {
+    internal::FEEvaluationImplBasisChange<
+      internal::evaluate_general,
+      dim,
+      degree_coarse + 1,
+      degree_fine + 1,
+      1,
+      Number,
+      Number>::do_forward(prolongation_matrix_1d,
+                          evaluation_data_coarse,
+                          evaluation_data_fine);
+  }
+
+private:
+  const AlignedVector<Number> &prolongation_matrix_1d;
+  const Number *               evaluation_data_coarse;
+  Number *                     evaluation_data_fine;
+};
+
+template <int dim, typename Number>
+class CellRestrictor
+{
+public:
+  CellRestrictor(const AlignedVector<Number> &prolongation_matrix_1d,
+                 Number *                     evaluation_data_fine,
+                 Number *                     evaluation_data_coarse)
+    : prolongation_matrix_1d(prolongation_matrix_1d)
+    , evaluation_data_fine(evaluation_data_fine)
+    , evaluation_data_coarse(evaluation_data_coarse)
+  {}
+
+  template <int degree_fine, int degree_coarse>
+  void
+  run()
+  {
+    internal::FEEvaluationImplBasisChange<
+      internal::evaluate_general,
+      dim,
+      degree_coarse + 1,
+      degree_fine + 1,
+      1,
+      Number,
+      Number>::do_backward(prolongation_matrix_1d,
+                           false,
+                           evaluation_data_fine,
+                           evaluation_data_coarse);
+  }
+
+private:
+  const AlignedVector<Number> &prolongation_matrix_1d;
+  Number *                     evaluation_data_fine;
+  Number *                     evaluation_data_coarse;
+};
+
+template <typename Number>
+void
+TransferScheme<Number>::print() const
+{
+  std::cout << "weights:" << std::endl;
+  for (const auto w : weights)
+    std::cout << w << " ";
+  std::cout << std::endl;
+
+  std::cout << "level_dof_indices_fine:" << std::endl;
+  for (const auto w : level_dof_indices_fine)
+    std::cout << w << " ";
+  std::cout << std::endl;
+
+  std::cout << "level_dof_indices_coarse:" << std::endl;
+  for (const auto w : level_dof_indices_coarse)
+    std::cout << w << " ";
+  std::cout << std::endl;
+
+  std::cout << "prolongation_matrix_1d:" << std::endl;
+  for (const auto w : prolongation_matrix_1d)
+    std::cout << w[0] << " ";
+  std::cout << std::endl;
+}
+
+template <int dim, typename Number>
+void
+Transfer<dim, Number>::print_internal() const
+{
+  for (const auto &scheme : schemes)
+    scheme.print();
+}
+
+template <int dim, typename Number>
+void
+Transfer<dim, Number>::do_prolongate_add(
+  const unsigned int                                to_level,
+  LinearAlgebra::distributed::Vector<Number> &      dst,
+  const LinearAlgebra::distributed::Vector<Number> &src) const
+{
+  (void)to_level;
+
+  const unsigned int vec_size = VectorizedArray<Number>::n_array_elements;
+
+  this->vec_coarse.copy_locally_owned_data_from(src);
+  this->vec_coarse.update_ghost_values();
+  this->constraint_coarse.distribute(this->vec_coarse);
+  this->vec_coarse
+    .update_ghost_values(); // note: make sure that ghost values are set
+
+  this->vec_fine = Number(0.);
+
+  for (const auto &scheme : schemes)
+    {
+      AlignedVector<VectorizedArray<Number>> evaluation_data_fine(
+        scheme.n_cell_dofs_fine);
+      AlignedVector<VectorizedArray<Number>> evaluation_data_coarse(
+        scheme.n_cell_dofs_fine);
+
+      CellProlongator<dim, VectorizedArray<Number>> cell_prolongator(
+        scheme.prolongation_matrix_1d,
+        evaluation_data_coarse.begin(),
+        evaluation_data_fine.begin());
+      CellTransfer cell_transfer(scheme.degree_fine, scheme.degree_coarse);
+
+      for (unsigned int cell = 0; cell < scheme.n_cells_coarse;
+           cell += vec_size)
+        {
+          const unsigned int n_lanes = cell + vec_size > scheme.n_cells_coarse ?
+                                         scheme.n_cells_coarse - cell :
+                                         vec_size;
+
+          // read from source vector
+          {
+            const unsigned int *indices =
+              &scheme
+                 .level_dof_indices_coarse[cell * scheme.n_cell_dofs_coarse];
+            for (unsigned int v = 0; v < n_lanes; ++v)
+              {
+                for (unsigned int i = 0; i < scheme.n_cell_dofs_coarse; ++i)
+                  evaluation_data_coarse[i][v] =
+                    this->vec_coarse.local_element(indices[i]);
+                indices += scheme.n_cell_dofs_coarse;
+              }
+          }
+
+          // ---------------------------- coarse -----------------------------
+          cell_transfer.run(cell_prolongator);
+          // ------------------------------ fine -----------------------------
+
+          if (scheme.fine_element_is_continuous)
+            {
+              const Number *w = &scheme.weights[cell * scheme.n_cell_dofs_fine];
+              for (unsigned int v = 0; v < n_lanes; ++v)
+                {
+                  for (unsigned int i = 0; i < scheme.n_cell_dofs_fine; ++i)
+                    evaluation_data_fine[i][v] *= w[i];
+                  w += scheme.n_cell_dofs_fine;
+                }
+            }
+
+
+          // write into dst vector
+          {
+            const unsigned int *indices =
+              &scheme.level_dof_indices_fine[cell * scheme.n_cell_dofs_fine];
+            for (unsigned int v = 0; v < n_lanes; ++v)
+              {
+                for (unsigned int i = 0; i < scheme.n_cell_dofs_fine; ++i)
+                  this->vec_fine.local_element(indices[i]) +=
+                    evaluation_data_fine[i][v];
+                indices += scheme.n_cell_dofs_fine;
+              }
+          }
+        }
+    }
+
+  this->vec_coarse.zero_out_ghosts(); // clear ghost values; else compress in
+                                      // do_restrict_add does not work
+
+  if (schemes.front().fine_element_is_continuous)
+    this->vec_fine.compress(VectorOperation::add);
+
+  dst.copy_locally_owned_data_from(this->vec_fine);
+}
+
+template <int dim, typename Number>
+void
+Transfer<dim, Number>::do_restrict_add(
+  const unsigned int                                from_level,
+  LinearAlgebra::distributed::Vector<Number> &      dst,
+  const LinearAlgebra::distributed::Vector<Number> &src) const
+{
+  (void)from_level;
+
+  const unsigned int vec_size = VectorizedArray<Number>::n_array_elements;
+
+  this->vec_fine.copy_locally_owned_data_from(src);
+  this->vec_fine.update_ghost_values();
+
+  this->vec_coarse.copy_locally_owned_data_from(dst);
+
+  for (const auto &scheme : schemes)
+    {
+      AlignedVector<VectorizedArray<Number>> evaluation_data_fine(
+        scheme.n_cell_dofs_fine);
+      AlignedVector<VectorizedArray<Number>> evaluation_data_coarse(
+        scheme.n_cell_dofs_fine);
+
+      CellRestrictor<dim, VectorizedArray<Number>> cell_restrictor(
+        scheme.prolongation_matrix_1d,
+        evaluation_data_fine.begin(),
+        evaluation_data_coarse.begin());
+      CellTransfer cell_transfer(scheme.degree_fine, scheme.degree_coarse);
+
+      for (unsigned int cell = 0; cell < scheme.n_cells_coarse;
+           cell += vec_size)
+        {
+          const unsigned int n_lanes = cell + vec_size > scheme.n_cells_coarse ?
+                                         scheme.n_cells_coarse - cell :
+                                         vec_size;
+
+          // read from source vector
+          {
+            const unsigned int *indices =
+              &scheme.level_dof_indices_fine[cell * scheme.n_cell_dofs_fine];
+            for (unsigned int v = 0; v < n_lanes; ++v)
+              {
+                for (unsigned int i = 0; i < scheme.n_cell_dofs_fine; ++i)
+                  evaluation_data_fine[i][v] =
+                    this->vec_fine.local_element(indices[i]);
+                indices += scheme.n_cell_dofs_fine;
+              }
+          }
+
+          if (scheme.fine_element_is_continuous)
+            {
+              const Number *w = &scheme.weights[cell * scheme.n_cell_dofs_fine];
+              for (unsigned int v = 0; v < n_lanes; ++v)
+                {
+                  for (unsigned int i = 0; i < scheme.n_cell_dofs_fine; ++i)
+                    evaluation_data_fine[i][v] *= w[i];
+                  w += scheme.n_cell_dofs_fine;
+                }
+            }
+
+          // ------------------------------ fine -----------------------------
+          cell_transfer.run(cell_restrictor);
+          // ----------------------------- coarse ----------------------------
+
+
+          // write into dst vector
+          {
+            const unsigned int *indices =
+              &scheme
+                 .level_dof_indices_coarse[cell * scheme.n_cell_dofs_coarse];
+            for (unsigned int v = 0; v < n_lanes; ++v)
+              {
+                for (unsigned int i = 0; i < scheme.n_cell_dofs_coarse; ++i)
+                  constraint_coarse.distribute_local_to_global(
+                    partitioner_coarse->local_to_global(indices[i]),
+                    evaluation_data_coarse[i][v],
+                    this->vec_coarse);
+                indices += scheme.n_cell_dofs_coarse;
+              }
+          }
+        }
+    }
+
+  if (schemes.front().fine_element_is_continuous)
+    this->vec_coarse.compress(VectorOperation::add);
+
+  dst.copy_locally_owned_data_from(this->vec_coarse);
+}
+
+
+DEAL_II_NAMESPACE_CLOSE
+
+#endif
