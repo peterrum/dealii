28291deb922b27ed2627cefdce2329efe9f0d1a1
Peter Munch <peterrmuench@gmail.com>
2020-07-21 13:03:00 +0200

    Generalize FiniteElement and FiniteElementData

--- include/deal.II/fe/fe_dgq.h
+++ include/deal.II/fe/fe_dgq.h
@@ -152,7 +152,8 @@ public:
    */
   virtual void
   get_face_interpolation_matrix(const FiniteElement<dim, spacedim> &source,
-                                FullMatrix<double> &matrix) const override;
+                                FullMatrix<double> &                matrix,
+                                const unsigned int face_no) const override;
 
   /**
    * Return the matrix interpolating from a face of one element to the face
@@ -168,7 +169,8 @@ public:
   virtual void
   get_subface_interpolation_matrix(const FiniteElement<dim, spacedim> &source,
                                    const unsigned int                  subface,
-                                   FullMatrix<double> &matrix) const override;
+                                   FullMatrix<double> &                matrix,
+                                   const unsigned int face_no) const override;
 
   /**
    * Projection from a fine grid space onto a coarse grid space. Overrides the
@@ -266,8 +268,8 @@ public:
    * course empty.
    */
   virtual std::vector<std::pair<unsigned int, unsigned int>>
-  hp_quad_dof_identities(
-    const FiniteElement<dim, spacedim> &fe_other) const override;
+  hp_quad_dof_identities(const FiniteElement<dim, spacedim> &fe_other,
+                         const unsigned int face_no) const override;
 
   /**
    * Return whether this element implements its hanging node constraints in
