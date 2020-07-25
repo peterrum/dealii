27a1d52742590e38f9d629bd7d7b47f433e96705
Peter Munch <peterrmuench@gmail.com>
2020-07-25 18:36:18 +0200

    Work on conversion

--- source/fe/fe_data.cc
+++ source/fe/fe_data.cc
@@ -19,6 +19,107 @@
 
 DEAL_II_NAMESPACE_OPEN
 
+namespace
+{
+  PrecomputedFiniteElementData
+  expand(const unsigned int               dim,
+         const std::vector<unsigned int> &dofs_per_object,
+         const ReferenceCell::Type        cell_type)
+  {
+    PrecomputedFiniteElementData result;
+
+    const unsigned int face_no = 0;
+
+    result.dofs_per_object_exclusive.resize(4, std::vector<unsigned int>(1));
+    result.dofs_per_object_inclusive.resize(4, std::vector<unsigned int>(1));
+    result.first_index.resize(4, std::vector<unsigned int>(1));
+    result.first_face_index.resize(3, std::vector<unsigned int>(1));
+
+    // dofs_per_vertex
+    const unsigned int dofs_per_vertex     = dofs_per_object[0];
+    result.dofs_per_object_exclusive[0][0] = dofs_per_vertex;
+
+    // dofs_per_line
+    const unsigned int dofs_per_line       = dofs_per_object[1];
+    result.dofs_per_object_exclusive[1][0] = dofs_per_line;
+
+    // dofs_per_quad
+    const unsigned int dofs_per_quad       = dofs_per_object[2];
+    result.dofs_per_object_exclusive[2][0] = dofs_per_quad;
+
+    // dofs_per_hex
+    const unsigned int dofs_per_hex        = dofs_per_object[3];
+    result.dofs_per_object_exclusive[3][0] = dofs_per_hex;
+
+
+    // first_line_index
+    const unsigned int first_line_index =
+      (ReferenceCell::internal::Info::get_cell(cell_type).n_vertices() *
+       dofs_per_vertex);
+    result.first_index[1][0] = first_line_index;
+
+    // first_quad_index
+    const unsigned int first_quad_index =
+      (first_line_index +
+       ReferenceCell::internal::Info::get_cell(cell_type).n_lines() *
+         dofs_per_line);
+    result.first_index[2][0] = first_quad_index;
+
+    // first_hex_index
+    result.first_index[3][0] =
+      (first_quad_index +
+       (dim == 2 ?
+          1 :
+          (dim == 3 ?
+             ReferenceCell::internal::Info::get_cell(cell_type).n_faces() :
+             0)) *
+         dofs_per_quad);
+
+    // first_face_line_index
+    result.first_face_index[1][0] =
+      (ReferenceCell::internal::Info::get_face(cell_type, face_no)
+         .n_vertices() *
+       dofs_per_vertex);
+
+    // first_face_quad_index
+    result.first_face_index[2][0] =
+      ((dim == 3 ?
+          ReferenceCell::internal::Info::get_face(cell_type, face_no)
+              .n_vertices() *
+            dofs_per_vertex :
+          ReferenceCell::internal::Info::get_cell(cell_type).n_vertices() *
+            dofs_per_vertex) +
+       ReferenceCell::internal::Info::get_face(cell_type, face_no).n_lines() *
+         dofs_per_line);
+
+    // dofs_per_face
+    result.dofs_per_object_inclusive[dim - 1][0] =
+      (ReferenceCell::internal::Info::get_face(cell_type, face_no)
+           .n_vertices() *
+         dofs_per_vertex +
+       ReferenceCell::internal::Info::get_face(cell_type, face_no).n_lines() *
+         dofs_per_line +
+       (dim == 3 ? 1 : 0) * dofs_per_quad);
+
+
+    // dofs_per_cell
+    result.dofs_per_object_inclusive[dim][0] =
+      (ReferenceCell::internal::Info::get_cell(cell_type).n_vertices() *
+         dofs_per_vertex +
+       ReferenceCell::internal::Info::get_cell(cell_type).n_lines() *
+         dofs_per_line +
+       (dim == 2 ?
+          1 :
+          (dim == 3 ?
+             ReferenceCell::internal::Info::get_cell(cell_type).n_faces() :
+             0)) *
+         dofs_per_quad +
+       (dim == 3 ? 1 : 0) * dofs_per_hex);
+
+    return result;
+  }
+} // namespace
+
 template <int dim>
 FiniteElementData<dim>::FiniteElementData(
   const std::vector<unsigned int> &dofs_per_object,
@@ -46,67 +147,13 @@ FiniteElementData<dim>::FiniteElementData(
   const unsigned int               degree,
   const Conformity                 conformity,
   const BlockIndices &             block_indices)
-  : cell_type(cell_type)
-  , dofs_per_vertex(dofs_per_object[0])
-  , dofs_per_line(dofs_per_object[1])
-  , dofs_per_quad(dim > 1 ? dofs_per_object[2] : 0)
-  , n_dofs_on_quad({{0, 0, 1, 1, 1}})
-  , dofs_per_hex(dim > 2 ? dofs_per_object[3] : 0)
-  , first_line_index(
-      ReferenceCell::internal::Info::get_cell(cell_type).n_vertices() *
-      dofs_per_vertex)
-  , first_quad_index(
-      first_line_index +
-      ReferenceCell::internal::Info::get_cell(cell_type).n_lines() *
-        dofs_per_line)
-  , first_hex_index(
-      first_quad_index +
-      (dim == 2 ?
-         1 :
-         (dim == 3 ?
-            ReferenceCell::internal::Info::get_cell(cell_type).n_faces() :
-            0)) *
-        dofs_per_quad)
-  , first_face_line_index(
-      ReferenceCell::internal::Info::get_face(cell_type, 0).n_vertices() *
-      dofs_per_vertex)
-  , first_face_quad_index(
-      (dim == 3 ?
-         ReferenceCell::internal::Info::get_face(cell_type, 0).n_vertices() *
-           dofs_per_vertex :
-         ReferenceCell::internal::Info::get_cell(cell_type).n_vertices() *
-           dofs_per_vertex) +
-      ReferenceCell::internal::Info::get_face(cell_type, 0).n_lines() *
-        dofs_per_line)
-  , dofs_per_face(
-      ReferenceCell::internal::Info::get_face(cell_type, 0).n_vertices() *
-        dofs_per_vertex +
-      ReferenceCell::internal::Info::get_face(cell_type, 0).n_lines() *
-        dofs_per_line +
-      (dim == 3 ? 1 : 0) * dofs_per_quad)
-  , n_dofs_on_face({{6, 6, 9, 9, 9}})
-  , dofs_per_cell(
-      ReferenceCell::internal::Info::get_cell(cell_type).n_vertices() *
-        dofs_per_vertex +
-      ReferenceCell::internal::Info::get_cell(cell_type).n_lines() *
-        dofs_per_line +
-      (dim == 2 ?
-         1 :
-         (dim == 3 ?
-            ReferenceCell::internal::Info::get_cell(cell_type).n_faces() :
-            0)) *
-        dofs_per_quad +
-      (dim == 3 ? 1 : 0) * dofs_per_hex)
-  , components(n_components)
-  , degree(degree)
-  , conforming_space(conformity)
-  , block_indices_data(block_indices.size() == 0 ?
-                         BlockIndices(1, dofs_per_cell) :
-                         block_indices)
-{
-  Assert(dofs_per_object.size() == dim + 1,
-         ExcDimensionMismatch(dofs_per_object.size() - 1, dim));
-}
+  : FiniteElementData(expand(dim, dofs_per_object, cell_type),
+                      cell_type,
+                      n_components,
+                      degree,
+                      conformity,
+                      block_indices)
+{}
 
 template <int dim>
 FiniteElementData<dim>::FiniteElementData(
