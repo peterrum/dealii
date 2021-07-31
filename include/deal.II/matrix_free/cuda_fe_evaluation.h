44db3cc675fa12b2628eb11e29cc0aa084cde274
Peter Munch <peterrmuench@gmail.com>
2021-07-31 11:28:26 +0200

    Swith from unsigned int to ConstraintTypes

--- include/deal.II/matrix_free/cuda_fe_evaluation.h
+++ include/deal.II/matrix_free/cuda_fe_evaluation.h
@@ -238,7 +238,7 @@ namespace CUDAWrappers
     unsigned int             padding_length;
     const unsigned int       mf_object_id;
 
-    const unsigned int constraint_mask;
+    const ConstraintTypes constraint_mask;
 
     const bool use_coloring;
 
