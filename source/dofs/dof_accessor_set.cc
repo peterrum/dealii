bd09a894200f63d25631f4d84ae56d99ec059b4e
Peter Munch <peterrmuench@gmail.com>
2022-02-27 07:26:49 +0100

    Simplify

--- source/dofs/dof_accessor_set.cc
+++ source/dofs/dof_accessor_set.cc
@@ -156,11 +156,14 @@ namespace internal
             class OutputVector,
             typename number>
   void
-  process_by_interpolation(const DoFCellAccessor<dim, spacedim, lda> &cell,
-                           const Vector<number> &local_values,
-                           OutputVector &        values,
-                           const unsigned int    fe_index_,
-                           const bool            perform_check)
+  process_by_interpolation(
+    const DoFCellAccessor<dim, spacedim, lda> &      cell,
+    const Vector<number> &                           local_values,
+    OutputVector &                                   values,
+    const unsigned int                               fe_index_,
+    const std::function<void(const DoFCellAccessor<dim, spacedim, lda> &cell,
+                             const Vector<number> &local_values,
+                             OutputVector &        values)> &processor)
   {
     const unsigned int fe_index =
       (cell.get_dof_handler().has_hp_capabilities() == false &&
@@ -177,10 +180,7 @@ namespace internal
             (fe_index == cell.active_fe_index()) ||
             (fe_index == DoFHandler<dim, spacedim>::invalid_fe_index))
           // simply set the values on this cell
-          internal::set_dof_values_with_check(cell,
-                                              local_values,
-                                              values,
-                                              perform_check);
+          processor(cell, local_values, values);
         else
           {
             Assert(local_values.size() ==
@@ -202,10 +202,7 @@ namespace internal
               interpolation.vmult(tmp, local_values);
 
             // now set the dof values in the global vector
-            internal::set_dof_values_with_check(cell,
-                                                tmp,
-                                                values,
-                                                perform_check);
+            processor(cell, tmp, values);
           }
       }
     else
@@ -239,10 +236,8 @@ namespace internal
             if (tmp.size() > 0)
               fe.get_prolongation_matrix(child, cell.refinement_case())
                 .vmult(tmp, local_values);
-            cell.child(child)->set_dof_values_by_interpolation(tmp,
-                                                               values,
-                                                               fe_index,
-                                                               perform_check);
+            process_by_interpolation(
+              *cell.child(child), tmp, values, fe_index, processor);
           }
       }
   }
@@ -260,8 +255,19 @@ DoFCellAccessor<dim, spacedim, lda>::set_dof_values_by_interpolation(
   const unsigned int    fe_index_,
   const bool            perform_check) const
 {
-  internal::process_by_interpolation(
-    *this, local_values, values, fe_index_, perform_check);
+  internal::process_by_interpolation<dim, spacedim, lda, OutputVector, number>(
+    *this,
+    local_values,
+    values,
+    fe_index_,
+    [perform_check](const DoFCellAccessor<dim, spacedim, lda> &cell,
+                    const Vector<number> &                     local_values,
+                    OutputVector &                             values) {
+      internal::set_dof_values_with_check(cell,
+                                          local_values,
+                                          values,
+                                          perform_check);
+    });
 }
 
 
@@ -274,95 +280,21 @@ DoFCellAccessor<dim, spacedim, lda>::
     OutputVector &        values,
     const unsigned int    fe_index_) const
 {
-  const unsigned int fe_index =
-    (this->dof_handler->hp_capability_enabled == false &&
-     fe_index_ == DoFHandler<dim, spacedim>::invalid_fe_index) ?
-      DoFHandler<dim, spacedim>::default_fe_index :
-      fe_index_;
-
-  if (this->is_active() && !this->is_artificial())
-    {
-      if ((this->dof_handler->hp_capability_enabled == false) ||
-          // for hp-DoFHandlers, we need to require that on
-          // active cells, you either don't specify an fe_index,
-          // or that you specify the correct one
-          (fe_index == this->active_fe_index()) ||
-          (fe_index == DoFHandler<dim, spacedim>::invalid_fe_index))
-        // simply set the values on this cell
-        {
-          std::vector<types::global_dof_index> dof_indices(
-            this->dof_handler->get_fe(fe_index).n_dofs_per_cell());
-          this->get_dof_indices(dof_indices);
-          AffineConstraints<number>().distribute_local_to_global(local_values,
-                                                                 dof_indices,
-                                                                 values);
-        }
-      else
-        {
-          Assert(local_values.size() ==
-                   this->dof_handler->get_fe(fe_index).n_dofs_per_cell(),
-                 ExcMessage("Incorrect size of local_values vector."));
-
-          FullMatrix<double> interpolation(
-            this->get_fe().n_dofs_per_cell(),
-            this->dof_handler->get_fe(fe_index).n_dofs_per_cell());
-
-          this->get_fe().get_interpolation_matrix(
-            this->dof_handler->get_fe(fe_index), interpolation);
-
-          // do the interpolation to the target space. for historical
-          // reasons, matrices are set to size 0x0 internally even
-          // we reinit as 4x0, so we have to treat this case specially
-          Vector<number> tmp(this->get_fe().n_dofs_per_cell());
-          if ((tmp.size() > 0) && (local_values.size() > 0))
-            interpolation.vmult(tmp, local_values);
-
-          // now set the dof values in the global vector
-          {
-            std::vector<types::global_dof_index> dof_indices(
-              this->dof_handler->get_fe(fe_index).n_dofs_per_cell());
-            this->get_dof_indices(dof_indices);
-            AffineConstraints<number>().distribute_local_to_global(tmp,
-                                                                   dof_indices,
-                                                                   values);
-          }
-        }
-    }
-  else
-    // otherwise distribute them to the children
-    {
-      Assert((this->dof_handler->hp_capability_enabled == false) ||
-               (fe_index != DoFHandler<dim, spacedim>::invalid_fe_index),
-             ExcMessage(
-               "You cannot call this function on non-active cells "
-               "of DoFHandler objects unless you provide an explicit "
-               "finite element index because they do not have naturally "
-               "associated finite element spaces associated: degrees "
-               "of freedom are only distributed on active cells for which "
-               "the active FE index has been set."));
-
-      const FiniteElement<dim, spacedim> &fe =
-        this->get_dof_handler().get_fe(fe_index);
-      const unsigned int dofs_per_cell = fe.n_dofs_per_cell();
-
-      Assert(this->dof_handler != nullptr,
-             typename BaseClass::ExcInvalidObject());
-      Assert(local_values.size() == dofs_per_cell,
-             typename BaseClass::ExcVectorDoesNotMatch());
-      Assert(values.size() == this->dof_handler->n_dofs(),
-             typename BaseClass::ExcVectorDoesNotMatch());
-
-      Vector<number> tmp(dofs_per_cell);
-
-      for (unsigned int child = 0; child < this->n_children(); ++child)
-        {
-          if (tmp.size() > 0)
-            fe.get_prolongation_matrix(child, this->refinement_case())
-              .vmult(tmp, local_values);
-          this->child(child)->distribute_local_to_global_by_interpolation(
-            tmp, values, fe_index);
-        }
-    }
+  internal::process_by_interpolation<dim, spacedim, lda, OutputVector, number>(
+    *this,
+    local_values,
+    values,
+    fe_index_,
+    [](const DoFCellAccessor<dim, spacedim, lda> &cell,
+       const Vector<number> &                     local_values,
+       OutputVector &                             values) {
+      std::vector<types::global_dof_index> dof_indices(
+        cell.get_fe().n_dofs_per_cell());
+      cell.get_dof_indices(dof_indices);
+      AffineConstraints<number>().distribute_local_to_global(local_values,
+                                                             dof_indices,
+                                                             values);
+    });
 }
 
 
