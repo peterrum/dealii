00d6cf5da48b888f944475071d26f3b65af9c2d8
Peter Munch <peterrmuench@gmail.com>
2022-07-04 22:01:14 +0200

    Add functions to restrict a matrix

---
+++ include/deal.II/lac/sparse_matrix_tools.h
@@ -0,0 +1,161 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 1999 - 2022 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+#ifndef dealii_sparse_matrix_tools_h
+#define dealii_sparse_matrix_tools_h
+
+#include <deal.II/base/config.h>
+
+#include <deal.II/base/mpi_compute_index_owner_internal.h>
+
+DEAL_II_NAMESPACE_OPEN
+
+namespace SparseMatrixTools
+{
+  template <int dim, int spacedim, typename SparseMatrixType, typename Number>
+  void
+  restrict_to_cells(const SparseMatrixType &         system_matrix,
+                    const DoFHandler<dim, spacedim> &dof_handler,
+                    std::vector<FullMatrix<Number>> &blocks)
+  {
+    const unsigned int dofs_per_cell = dof_handler.get_fe().n_dofs_per_cell();
+
+    std::vector<types::global_dof_index> local_dof_indices(dofs_per_cell);
+
+    const auto locally_owned_dofs = dof_handler.locally_owned_dofs();
+
+    IndexSet locally_active_dofs;
+    DoFTools::extract_locally_active_dofs(dof_handler, locally_active_dofs);
+
+    locally_active_dofs.subtract_set(locally_owned_dofs);
+
+    const auto comm = dof_handler.get_communicator();
+
+    std::vector<unsigned int> dummy(locally_active_dofs.n_elements());
+
+    Utilities::MPI::internal::ComputeIndexOwner::ConsensusAlgorithmsPayload
+      process(locally_owned_dofs, locally_active_dofs, comm, dummy, true);
+
+    Utilities::MPI::ConsensusAlgorithms::Selector<
+      std::vector<std::pair<types::global_dof_index, types::global_dof_index>>,
+      std::vector<unsigned int>>
+      consensus_algorithm;
+    consensus_algorithm.run(process, comm);
+
+    using T1 = std::vector<
+      std::pair<types::global_dof_index,
+                std::vector<std::pair<types::global_dof_index, Number>>>>;
+
+    auto requesters = process.get_requesters();
+
+    std::vector<std::vector<std::pair<types::global_dof_index, Number>>>
+      locally_relevant_matrix_entries(locally_active_dofs.n_elements());
+
+
+    std::vector<unsigned int> ranks;
+
+    for (const auto &i : requesters)
+      ranks.push_back(i.first);
+
+    dealii::Utilities::MPI::ConsensusAlgorithms::selector<T1>(
+      ranks,
+      [&](const unsigned int other_rank) {
+        T1 send_buffer;
+
+        for (auto index : requesters[other_rank])
+          {
+            std::vector<std::pair<types::global_dof_index, Number>> t;
+
+            for (auto entry = system_matrix.begin(index);
+                 entry != system_matrix.end(index);
+                 ++entry)
+              t.emplace_back(entry->column(), entry->value());
+
+            send_buffer.emplace_back(index, t);
+          }
+
+        return send_buffer;
+      },
+      [&](const unsigned int &, const T1 &buffer_recv) {
+        for (const auto &i : buffer_recv)
+          {
+            auto &dst =
+              locally_relevant_matrix_entries[locally_active_dofs
+                                                .index_within_set(i.first)];
+            dst = i.second;
+            std::sort(dst.begin(), dst.end(), [](const auto &a, const auto &b) {
+              return a.first < b.first;
+            });
+          }
+      },
+      comm);
+
+    blocks.clear();
+    blocks.resize(dof_handler.get_triangulation().n_active_cells());
+
+    for (const auto &cell : dof_handler.active_cell_iterators())
+      {
+        if (cell->is_locally_owned() == false)
+          continue;
+
+        cell->get_dof_indices(local_dof_indices);
+
+        auto &cell_matrix = blocks[cell->active_cell_index()];
+
+        cell_matrix = FullMatrix<Number>(dofs_per_cell, dofs_per_cell);
+
+        for (unsigned int i = 0; i < dofs_per_cell; ++i)
+          for (unsigned int j = 0; j < dofs_per_cell; ++j)
+            {
+              if (locally_owned_dofs.is_element(
+                    local_dof_indices[i])) // row is local
+                {
+                  cell_matrix(i, j) =
+                    system_matrix(local_dof_indices[i], local_dof_indices[j]);
+                }
+              else // row is ghost
+                {
+                  Assert(locally_active_dofs.is_element(local_dof_indices[i]),
+                         ExcInternalError());
+
+                  const auto &row_entries =
+                    locally_relevant_matrix_entries[locally_active_dofs
+                                                      .index_within_set(
+                                                        local_dof_indices[i])];
+
+                  const auto ptr =
+                    std::lower_bound(row_entries.begin(),
+                                     row_entries.end(),
+                                     std::pair<types::global_dof_index, Number>{
+                                       local_dof_indices[j], /*dummy*/ 0.0},
+                                     [](const auto a, const auto b) {
+                                       return a.first < b.first;
+                                     });
+
+                  Assert(ptr != row_entries.end() &&
+                           local_dof_indices[j] == ptr->first,
+                         ExcInternalError());
+
+                  cell_matrix(i, j) = ptr->second;
+                }
+            }
+      }
+  }
+
+} // namespace SparseMatrixTools
+
+DEAL_II_NAMESPACE_CLOSE
+
+#endif
---
+++ tests/lac/sparse_matrix_tools_01.cc
@@ -0,0 +1,95 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2022 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+#include <deal.II/base/quadrature_lib.h>
+
+#include <deal.II/distributed/tria.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_q.h>
+
+#include <deal.II/grid/grid_generator.h>
+
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/sparse_matrix_tools.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+#include <deal.II/lac/trilinos_sparsity_pattern.h>
+
+#include <deal.II/numerics/matrix_creator.h>
+
+#include "../tests.h"
+
+template <int dim>
+void
+test()
+{
+  const unsigned int fe_degree = 1;
+
+  // create mesh, ...
+  parallel::distributed::Triangulation<dim> tria(MPI_COMM_WORLD);
+  GridGenerator::subdivided_hyper_cube(tria, 3);
+
+  DoFHandler<dim> dof_handler(tria);
+  dof_handler.distribute_dofs(FE_Q<dim>(fe_degree));
+
+  QGauss<dim> quadrature(fe_degree + 1);
+
+  AffineConstraints<double> constraints;
+  // DoFTools::make_zero_boundary_constraints (dof_handler, constraints);
+  constraints.close();
+
+  // create system matrix
+  TrilinosWrappers::SparsityPattern sparsity_pattern(
+    dof_handler.locally_owned_dofs(), dof_handler.get_communicator());
+  DoFTools::make_sparsity_pattern(dof_handler,
+                                  sparsity_pattern,
+                                  constraints,
+                                  false);
+  sparsity_pattern.compress();
+
+  TrilinosWrappers::SparseMatrix laplace_matrix;
+  laplace_matrix.reinit(sparsity_pattern);
+
+  MatrixCreator::
+    create_laplace_matrix<dim, dim, TrilinosWrappers::SparseMatrix>(
+      dof_handler, quadrature, laplace_matrix, nullptr, constraints);
+
+  // extract blocks
+  std::vector<FullMatrix<double>> blocks;
+  SparseMatrixTools::restrict_to_cells(laplace_matrix, dof_handler, blocks);
+
+  for (const auto &block : blocks)
+    {
+      if (block.m() == 0 && block.m() == 0)
+        continue;
+
+      block.print_formatted(deallog.get_file_stream(), 2, false, 8);
+      deallog << std::endl;
+    }
+}
+
+#include "../tests.h"
+
+int
+main(int argc, char *argv[])
+{
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+
+  MPILogInitAll all;
+
+  test<2>();
+}
