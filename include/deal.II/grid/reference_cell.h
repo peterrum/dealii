1499f8c6aacf9ef66a1da2e3843dee48f2f83690
Peter Munch <peterrmuench@gmail.com>
2023-10-08 16:48:45 +0200

    Continue

--- include/deal.II/grid/reference_cell.h
+++ include/deal.II/grid/reference_cell.h
@@ -54,7 +54,7 @@ namespace internal
      {{false, true, false, true, false, true}},
      {{false, true, true, false, false, true}},
      {{true, false, false, true, true, false}},
-     {{false, true, true, true, true, true}}}};
+     {{false, true, false, true, true, true}}}};
 
   /**
    * A helper function to create a ReferenceCell object from an integer.
