4f7c773102c5a71a4d6886f3c3e2bf654a953ad5
Peter Munch <peterrmuench@gmail.com>
2025-01-20 13:33:03 +0100

    Add step-96

---
+++ examples/step-96/step-96.cc
@@ -0,0 +1,26 @@
+/* ------------------------------------------------------------------------
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * Copyright (C) 2025 by the deal.II authors
+ *
+ * This file is part of the deal.II library.
+ *
+ * Part of the source code is dual licensed under Apache-2.0 WITH
+ * LLVM-exception OR LGPL-2.1-or-later. Detailed license information
+ * governing the source code and code contributions can be found in
+ * LICENSE.md and CONTRIBUTING.md at the top level directory of deal.II.
+ *
+ * ------------------------------------------------------------------------
+ *
+ *
+ * Authors: Camilla Belponer, University of Augsburg, 2025
+ *          Peter Munch, Technical University of Berlin, 2025
+ */
+
+// @sect3{Include files}
+//
+// The program starts with including all the relevant header files.
+#include <deal.II/base/config.h>
+
+int main()
+{}
