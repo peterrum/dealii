e8e09cd9ea590d2b94cb35a0e3bc9343ab188ff2
Peter Munch <peterrmuench@gmail.com>
2025-10-21 08:39:04 +0200

    Test more

--- tests/matrix_free/ecl_06.cc
+++ tests/matrix_free/ecl_06.cc
@@ -33,7 +33,7 @@
 #include "../tests.h"
 
 // Compare the evaluation of a SIP Laplace operator with face- and element-
-// centric loops on a hypercube, hypershell, and hyperball.
+// centric loops on a hypercube, hypershell, and hyperball and p adaptivity.
 
 template <int dim, typename Number>
 class CosineFunction : public Function<dim, Number>
@@ -47,15 +47,13 @@ public:
   }
 };
 
-template <int dim,
-          typename Number              = double,
-          typename VectorizedArrayType = VectorizedArray<Number, 1>>
+template <int dim, typename Number, typename VectorizedArrayType>
 void
 test(const unsigned int geometry,
      const int          fe_degree,
+     const bool         run_ecl,
      const unsigned int n_refinements = 1,
-     const bool         print_vector  = true,
-     const MPI_Comm     comm          = MPI_COMM_SELF)
+     const bool         print_vector  = true)
 {
   using VectorType = LinearAlgebra::distributed::Vector<Number>;
 
@@ -108,14 +106,24 @@ test(const unsigned int geometry,
     update_values | update_gradients;
   additional_data.tasks_parallel_scheme =
     MF::AdditionalData::TasksParallelScheme::none;
-  additional_data.hold_all_faces_to_owned_cells        = true;
-  additional_data.cell_vectorization_categories_strict = true;
-  additional_data.mapping_update_flags_faces_by_cells =
-    additional_data.mapping_update_flags_inner_faces |
-    additional_data.mapping_update_flags_boundary_faces;
-  additional_data.communicator_sm = comm;
 
-  // MatrixFreeTools::categorize_by_boundary_ids(tria, additional_data);
+  if (run_ecl)
+    {
+      AssertDimension(VectorizedArrayType::size(), 1);
+
+      // note: catergorization and hp is not working together, i.e., we
+      // cannot categorize the cells according to the boundary conditions
+      // needed for ECL (TODO)
+
+      additional_data.hold_all_faces_to_owned_cells        = true;
+      additional_data.cell_vectorization_categories_strict = true;
+      additional_data.mapping_update_flags_faces_by_cells =
+        additional_data.mapping_update_flags_inner_faces |
+        additional_data.mapping_update_flags_boundary_faces;
+
+      // MatrixFreeTools::categorize_by_boundary_ids(tria, additional_data);
+    }
+
 
   MF matrix_free;
   matrix_free.reinit(mapping, dof_handler, constraint, quad, additional_data);
@@ -242,154 +250,163 @@ test(const unsigned int geometry,
   if (print_vector)
     dst.print(deallog.get_file_stream());
 
-  dst = 0.0;
+  if (run_ecl)
+    {
+      dst = 0.0;
 
-  /**
-   * Element-centric loop
-   */
-  matrix_free.template loop_cell_centric<VectorType, VectorType>(
-    [&](const auto &, auto &dst, const auto &src, const auto range) {
-      const unsigned int active_fe_index_m =
-        matrix_free.get_cell_iterator(range.first, 0)->active_fe_index();
+      /**
+       * Element-centric loop
+       */
+      matrix_free.template loop_cell_centric<VectorType, VectorType>(
+        [&](const auto &, auto &dst, const auto &src, const auto range) {
+          const unsigned int active_fe_index_m =
+            matrix_free.get_cell_iterator(range.first, 0)->active_fe_index();
 
-      FEEvaluation<dim, -1, 0, 1, Number, VectorizedArrayType> phi(
-        matrix_free, 0, 0, 0, active_fe_index_m, active_fe_index_m);
+          FEEvaluation<dim, -1, 0, 1, Number, VectorizedArrayType> phi(
+            matrix_free, 0, 0, 0, active_fe_index_m, active_fe_index_m);
 
-      Table<2,
-            std::shared_ptr<
-              FEFaceEvaluation<dim, -1, 0, 1, Number, VectorizedArrayType>>>
-        phi_ms(2, 2);
+          Table<2,
+                std::shared_ptr<
+                  FEFaceEvaluation<dim, -1, 0, 1, Number, VectorizedArrayType>>>
+            phi_ms(2, 2);
 
-      for (unsigned int q = 0; q < quad.size(); ++q)
-        for (unsigned int i = 0;
-             i < matrix_free.get_dof_handler().get_fe_collection().size();
-             ++i)
-          phi_ms[i][q] = std::make_shared<
-            FEFaceEvaluation<dim, -1, 0, 1, Number, VectorizedArrayType>>(
-            matrix_free, true, 0, 0, 0, i, q);
+          for (unsigned int q = 0; q < quad.size(); ++q)
+            for (unsigned int i = 0;
+                 i < matrix_free.get_dof_handler().get_fe_collection().size();
+                 ++i)
+              phi_ms[i][q] = std::make_shared<
+                FEFaceEvaluation<dim, -1, 0, 1, Number, VectorizedArrayType>>(
+                matrix_free, true, 0, 0, 0, i, q);
 
-      Table<2,
-            std::shared_ptr<
-              FEFaceEvaluation<dim, -1, 0, 1, Number, VectorizedArrayType>>>
-        phi_ps(2, 2);
+          Table<2,
+                std::shared_ptr<
+                  FEFaceEvaluation<dim, -1, 0, 1, Number, VectorizedArrayType>>>
+            phi_ps(2, 2);
 
-      for (unsigned int q = 0; q < quad.size(); ++q)
-        for (unsigned int i = 0;
-             i < matrix_free.get_dof_handler().get_fe_collection().size();
-             ++i)
-          phi_ps[i][q] = std::make_shared<
-            FEFaceEvaluation<dim, -1, 0, 1, Number, VectorizedArrayType>>(
-            matrix_free, false, 0, 0, 0, i, q);
+          for (unsigned int q = 0; q < quad.size(); ++q)
+            for (unsigned int i = 0;
+                 i < matrix_free.get_dof_handler().get_fe_collection().size();
+                 ++i)
+              phi_ps[i][q] = std::make_shared<
+                FEFaceEvaluation<dim, -1, 0, 1, Number, VectorizedArrayType>>(
+                matrix_free, false, 0, 0, 0, i, q);
 
-      for (unsigned int cell = range.first; cell < range.second; ++cell)
-        {
-          phi.reinit(cell);
-          phi.read_dof_values(src);
-          phi.evaluate(EvaluationFlags::gradients);
-          for (unsigned int q = 0; q < phi.n_q_points; ++q)
-            phi.submit_gradient(phi.get_gradient(q), q);
-          phi.integrate(EvaluationFlags::gradients);
-
-          for (unsigned int face = 0; face < GeometryInfo<dim>::faces_per_cell;
-               ++face)
+          for (unsigned int cell = range.first; cell < range.second; ++cell)
             {
-              auto bids =
-                matrix_free.get_faces_by_cells_boundary_id(cell, face);
+              phi.reinit(cell);
+              phi.read_dof_values(src);
+              phi.evaluate(EvaluationFlags::gradients);
+              for (unsigned int q = 0; q < phi.n_q_points; ++q)
+                phi.submit_gradient(phi.get_gradient(q), q);
+              phi.integrate(EvaluationFlags::gradients);
 
-              if (bids[0] != numbers::internal_face_boundary_id)
+              for (unsigned int face = 0;
+                   face < GeometryInfo<dim>::faces_per_cell;
+                   ++face)
                 {
-                  auto &phi_m = *phi_ms[active_fe_index_m][active_fe_index_m];
+                  auto bids =
+                    matrix_free.get_faces_by_cells_boundary_id(cell, face);
 
-                  phi_m.reinit(cell, face);
-                  phi_m.read_dof_values(src);
-                  phi_m.evaluate(EvaluationFlags::values |
-                                 EvaluationFlags::gradients);
-                  VectorizedArrayType sigmaF =
-                    std::abs((phi_m.normal_vector(0) *
-                              phi_m.inverse_jacobian(0))[dim - 1]) *
-                    Number(std::max(fe_degree, 1) * (fe_degree + 1.0)) * 2.0;
-
-                  for (unsigned int q = 0; q < phi_m.n_q_points; ++q)
+                  if (bids[0] != numbers::internal_face_boundary_id)
                     {
-                      VectorizedArrayType average_value = phi_m.get_value(q);
-                      VectorizedArrayType average_valgrad =
-                        -phi_m.get_normal_derivative(q);
-                      average_valgrad += average_value * sigmaF * 2.0;
-                      phi_m.submit_normal_derivative(-average_value, q);
-                      phi_m.submit_value(average_valgrad, q);
+                      auto &phi_m =
+                        *phi_ms[active_fe_index_m][active_fe_index_m];
+
+                      phi_m.reinit(cell, face);
+                      phi_m.read_dof_values(src);
+                      phi_m.evaluate(EvaluationFlags::values |
+                                     EvaluationFlags::gradients);
+                      VectorizedArrayType sigmaF =
+                        std::abs((phi_m.normal_vector(0) *
+                                  phi_m.inverse_jacobian(0))[dim - 1]) *
+                        Number(std::max(fe_degree, 1) * (fe_degree + 1.0)) *
+                        2.0;
+
+                      for (unsigned int q = 0; q < phi_m.n_q_points; ++q)
+                        {
+                          VectorizedArrayType average_value =
+                            phi_m.get_value(q);
+                          VectorizedArrayType average_valgrad =
+                            -phi_m.get_normal_derivative(q);
+                          average_valgrad += average_value * sigmaF * 2.0;
+                          phi_m.submit_normal_derivative(-average_value, q);
+                          phi_m.submit_value(average_valgrad, q);
+                        }
+
+                      phi_m.integrate(EvaluationFlags::values |
+                                      EvaluationFlags::gradients);
+
+                      AssertDimension(phi.dofs_per_cell, phi_m.dofs_per_cell);
+
+                      for (unsigned int q = 0; q < phi.dofs_per_cell; ++q)
+                        phi.begin_dof_values()[q] +=
+                          phi_m.begin_dof_values()[q];
                     }
-
-                  phi_m.integrate(EvaluationFlags::values |
-                                  EvaluationFlags::gradients);
-
-                  AssertDimension(phi.dofs_per_cell, phi_m.dofs_per_cell);
-
-                  for (unsigned int q = 0; q < phi.dofs_per_cell; ++q)
-                    phi.begin_dof_values()[q] += phi_m.begin_dof_values()[q];
-                }
-              else
-                {
-                  const unsigned int active_fe_index_p =
-                    matrix_free.get_cell_iterator(cell, 0)
-                      ->neighbor(face)
-                      ->active_fe_index();
-                  const unsigned int active_q_index =
-                    std::max(active_fe_index_m, active_fe_index_p);
-
-                  auto &phi_m = *phi_ms[active_fe_index_m][active_q_index];
-                  auto &phi_p = *phi_ps[active_fe_index_p][active_q_index];
-
-                  phi_m.reinit(cell, face);
-                  phi_p.reinit(cell, face);
-
-                  phi_m.read_dof_values(src);
-                  phi_m.evaluate(EvaluationFlags::values |
-                                 EvaluationFlags::gradients);
-                  phi_p.read_dof_values(src);
-                  phi_p.evaluate(EvaluationFlags::values |
-                                 EvaluationFlags::gradients);
-
-                  VectorizedArrayType sigmaF =
-                    (std::abs((phi_m.normal_vector(0) *
-                               phi_m.inverse_jacobian(0))[dim - 1]) +
-                     std::abs((phi_m.normal_vector(0) *
-                               phi_p.inverse_jacobian(0))[dim - 1])) *
-                    (Number)(std::max(fe_degree, 1) * (fe_degree + 1.0));
-
-                  for (unsigned int q = 0; q < phi_m.n_q_points; ++q)
+                  else
                     {
-                      VectorizedArrayType average_value =
-                        (phi_m.get_value(q) - phi_p.get_value(q)) * 0.5;
-                      VectorizedArrayType average_valgrad =
-                        phi_m.get_normal_derivative(q) +
-                        phi_p.get_normal_derivative(q);
-                      average_valgrad =
-                        average_value * 2. * sigmaF - average_valgrad * 0.5;
-                      phi_m.submit_normal_derivative(-average_value, q);
-                      phi_m.submit_value(average_valgrad, q);
-                    }
-                  phi_m.integrate(EvaluationFlags::values |
-                                  EvaluationFlags::gradients);
+                      const unsigned int active_fe_index_p =
+                        matrix_free.get_cell_iterator(cell, 0)
+                          ->neighbor(face)
+                          ->active_fe_index();
+                      const unsigned int active_q_index =
+                        std::max(active_fe_index_m, active_fe_index_p);
 
-                  for (unsigned int q = 0; q < phi.dofs_per_cell; ++q)
-                    phi.begin_dof_values()[q] += phi_m.begin_dof_values()[q];
+                      auto &phi_m = *phi_ms[active_fe_index_m][active_q_index];
+                      auto &phi_p = *phi_ps[active_fe_index_p][active_q_index];
+
+                      phi_m.reinit(cell, face);
+                      phi_p.reinit(cell, face);
+
+                      phi_m.read_dof_values(src);
+                      phi_m.evaluate(EvaluationFlags::values |
+                                     EvaluationFlags::gradients);
+                      phi_p.read_dof_values(src);
+                      phi_p.evaluate(EvaluationFlags::values |
+                                     EvaluationFlags::gradients);
+
+                      VectorizedArrayType sigmaF =
+                        (std::abs((phi_m.normal_vector(0) *
+                                   phi_m.inverse_jacobian(0))[dim - 1]) +
+                         std::abs((phi_m.normal_vector(0) *
+                                   phi_p.inverse_jacobian(0))[dim - 1])) *
+                        (Number)(std::max(fe_degree, 1) * (fe_degree + 1.0));
+
+                      for (unsigned int q = 0; q < phi_m.n_q_points; ++q)
+                        {
+                          VectorizedArrayType average_value =
+                            (phi_m.get_value(q) - phi_p.get_value(q)) * 0.5;
+                          VectorizedArrayType average_valgrad =
+                            phi_m.get_normal_derivative(q) +
+                            phi_p.get_normal_derivative(q);
+                          average_valgrad =
+                            average_value * 2. * sigmaF - average_valgrad * 0.5;
+                          phi_m.submit_normal_derivative(-average_value, q);
+                          phi_m.submit_value(average_valgrad, q);
+                        }
+                      phi_m.integrate(EvaluationFlags::values |
+                                      EvaluationFlags::gradients);
+
+                      for (unsigned int q = 0; q < phi.dofs_per_cell; ++q)
+                        phi.begin_dof_values()[q] +=
+                          phi_m.begin_dof_values()[q];
+                    }
                 }
+
+              phi.set_dof_values(dst);
             }
+        },
+        dst,
+        src,
+        false,
+        MF::DataAccessOnFaces::gradients);
 
-          phi.set_dof_values(dst);
-        }
-    },
-    dst,
-    src,
-    false,
-    MF::DataAccessOnFaces::gradients);
+      deallog << dst.l2_norm() << std::endl;
 
-  deallog << dst.l2_norm() << std::endl;
+      if (print_vector)
+        dst.print(deallog.get_file_stream());
 
-  if (print_vector)
-    dst.print(deallog.get_file_stream());
-
-  std::cout << std::endl;
+      std::cout << std::endl;
+    }
 }
 
 int
@@ -398,7 +415,9 @@ main(int argc, char **argv)
   Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv, 1);
 
   mpi_initlog();
-  test<2, double>(0, 2);
-  test<2, double>(1, 2);
-  test<2, double>(2, 2);
+  for (unsigned int i = 0; i < 3; ++i)
+    {
+      test<2, double, VectorizedArray<double>>(i, 2, false);
+      test<2, double, VectorizedArray<double, 1>>(i, 2, true);
+    }
 }
